74eee6b62b1ba81bb819bca39ab6e54311a5b211
Ole Martin Handeland <git@olemartin.org>
2022-08-02 11:32:59 +0200

    Implementing more advanced unit test for JsonHelper

--- test/Altinn.App.Common.Tests/Helpers/JsonHelperTests/JsonHelperTests.cs
+++ test/Altinn.App.Common.Tests/Helpers/JsonHelperTests/JsonHelperTests.cs
@@ -1,7 +1,6 @@
 using Altinn.App.Common.Helpers;
 using FluentAssertions;
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace Altinn.App.Common.Tests.Helpers.JsonHelperTests
@@ -9,15 +8,50 @@ namespace Altinn.App.Common.Tests.Helpers.JsonHelperTests
     public class JsonHelperTests
     {
         [Fact]
-        public void FindChangedFields_RepeatingGroups_ShouldFindRemovedEntry()
+        public async void FindChangedFields_RepeatingGroups_ShouldFindRemovedEntry()
         {
-            var simpleJsonUnchanged = EmbeddedResource.LoadDataAsString("Altinn.App.Common.Tests.Helpers.JsonHelperTests.TestData.simple_unchanged.json");
-            var simpleJsonChanged = EmbeddedResource.LoadDataAsString("Altinn.App.Common.Tests.Helpers.JsonHelperTests.TestData.simple_changed.json");
+            var before = await EmbeddedResource.LoadDataAsString(
+                "Altinn.App.Common.Tests.Helpers.JsonHelperTests.TestData.before.json"
+            );
+            var after = await EmbeddedResource.LoadDataAsString(
+                "Altinn.App.Common.Tests.Helpers.JsonHelperTests.TestData.after.json"
+            );
 
-            Dictionary<string, object> changedFields = JsonHelper.FindChangedFields("{\"test\":\"to_be_removed\"}", "{}");
+            Dictionary<string, object> changedFields = JsonHelper.FindChangedFields(before, after);
 
-            changedFields.Should().HaveCount(1);
-            changedFields.First(e => e.Key == "test").Value.Should().Be(null);
+            changedFields.Should().HaveCount(18);
+            changedFields.Should().Equal(new Dictionary<string, object>
+            {
+                {"willBeRemoved", null},
+                {"willChangeValue", false},
+                {"object.willBeRemoved", null},
+                {"object.listInObject[2]", null},
+
+                // One item has been removed, so the later values have been shifted up
+                {"moreAdvanced.oneRemovedInList[2]", "kept3"},
+                {"moreAdvanced.oneRemovedInList[3]", "kept4"},
+                {"moreAdvanced.oneRemovedInList[4]", null},
+
+                {"moreAdvanced.objectWithRemovedProperty.removed1", null},
+
+                {"moreAdvanced.objectWithRemovedPropertyAndInnerChanges.removed1", null},
+                {"moreAdvanced.objectWithRemovedPropertyAndInnerChanges.removed2", null},
+                {"moreAdvanced.objectWithRemovedPropertyAndInnerChanges.kept1.removedProp", null},
+                {"moreAdvanced.objectWithRemovedPropertyAndInnerChanges.kept2.second", true},
+
+                {"moreAdvanced.mixedList[0].first", false},
+                {"moreAdvanced.mixedList[1].first", true},
+                {"moreAdvanced.mixedList[2]", "some string that changes"},
+
+                // This index used to be a number, but the number was removed, so what was [4] is now [3].
+                // We need to tell the client that the scalar value in [3] is no more, while also putting
+                // an object there to replace it.
+                // {"moreAdvanced.mixedList[3]", null}, // TODO: Implement
+                {"moreAdvanced.mixedList[3].first", "absolutely not"},
+
+                {"moreAdvanced.mixedList[4].first", null},
+                {"moreAdvanced.mixedList[4].otherRemovedProp", null},
+            });
         }
     }
 }
