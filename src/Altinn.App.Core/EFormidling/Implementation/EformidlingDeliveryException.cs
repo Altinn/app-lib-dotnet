c8cb049eba791ac14110c387221e1eba04f0f2d4
Ronny Birkeli <ronny.birkeli@digdir.no>
2022-10-26 10:10:24 +0200

    feat: Improve error handling for faling messages.

---
+++ src/Altinn.App.Core/EFormidling/Implementation/EformidlingDeliveryException.cs
@@ -0,0 +1,26 @@
+﻿namespace Altinn.App.Core.EFormidling.Implementation
+{
+    /// <summary>
+    /// Exception thrown when Eformidling is unable to process the message delivered to
+    /// the integration point.
+    /// </summary>
+    public class EformidlingDeliveryException : Exception
+    {
+        ///<inheritDoc/>
+        public EformidlingDeliveryException()
+        {
+        }
+
+        ///<inheritDoc/>
+        public EformidlingDeliveryException(string message)
+            : base(message)
+        {
+        }
+
+        ///<inheritDoc/>
+        public EformidlingDeliveryException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
--- src/Altinn.App.Core/EFormidling/Implementation/EformidlingStatusCheckEventHandler.cs
+++ src/Altinn.App.Core/EFormidling/Implementation/EformidlingStatusCheckEventHandler.cs
@@ -1,4 +1,5 @@
-﻿using Altinn.App.Core.Features;
+﻿using Altinn.App.Core.EFormidling.Implementation;
+using Altinn.App.Core.Features;
 using Altinn.App.Core.Interface;
 using Altinn.App.Core.Models;
 using Altinn.Common.EFormidlingClient;
@@ -40,37 +41,78 @@ namespace Altinn.App.Controllers
             
             // Instance GUID is used as shipment identifier
             string id = instanceIdentifier.InstanceGuid.ToString();
-            
-            if (await MessageDeliveredToKS(id))
+            Statuses statusesForShipment = await GetStatusesForShipment(id);
+            if (MessageDeliveredToKS(statusesForShipment))
             {
                 // Update status on instance if message is confirmed delivered to KS.
                 // The instance should wait in feedback step. This enforces a feedback step in the process in current version.
                 // Moving forward sending to Eformidling should considered as a ServiceTask with auto advance in the process
                 // when the message is confirmed.                
                 _ = await _instanceClient.AddCompleteConfirmation(instanceIdentifier.InstanceOwnerPartyId, instanceIdentifier.InstanceGuid);
-                // SelfLinkHelper.SetInstanceAppSelfLinks(instance, Request); //TODO: Look at how to avoid a dependency on Request
 
                 return true;
             }
+            else if (MessageMalformed(statusesForShipment, out string errorMalformed))
+            {
+                throw new EformidlingDeliveryException($"The message with id {id} was not delivered by Eformidling to KS. Error from Eformidling: {errorMalformed}.");
+            }
+            else if (MessageTimedOutToKS(statusesForShipment, out string errorTimeout))
+            {
+                throw new EformidlingDeliveryException($"The message with id {id} was not delivered by Eformidling to KS. The message lifetime has expired. Error from Eformidling: {errorTimeout}");
+            }
             else
             {
+                // The message isn't processed yet.
+                // We will try again later.
                 return false;
-            }            
-            
+            }
+                        
             // We don't know if this is the last reminder from the Event system. If the
             // Event system gives up (after 48 hours) it will end up in the dead letter queue,
             // and be handled by the Platform team manually.
         }
 
-        private async Task<bool> MessageDeliveredToKS(string shipmentId)
+        private async Task<Statuses> GetStatusesForShipment(string shipmentId)
         {
             var requestHeaders = new Dictionary<string, string>(); //TODO: Do we need any? Probably Authorization headers
 
             Statuses statuses = await _eFormidlingClient.GetMessageStatusById(shipmentId, requestHeaders);
 
-            _logger.LogInformation("Received {numberOf} statuses.", statuses.Content.Count);
+            _logger.LogInformation("Received the following {count} statuses: {statusValues}.", statuses.Content.Count, string.Join(",", statuses.Content.Select(s => s.Status).ToArray()));
 
+            return statuses;
+        }
+
+        private static bool MessageDeliveredToKS(Statuses statuses)
+        {
             return statuses.Content.FirstOrDefault(s => s.Status.ToLower() == "levert") != null;
+        }
+
+        private static bool MessageTimedOutToKS(Statuses statuses, out string errorMessage)
+        {
+            (bool error, errorMessage) = CheckErrorStatus(statuses, "levetid_utlopt");
+            return error;
+        }
+
+        private static bool MessageMalformed(Statuses statuses, out string errorMessage)
+        {
+            (bool error, errorMessage) = CheckErrorStatus(statuses, "feil");
+            return error;
+        }
+
+        private static (bool Error, string ErrorMessage) CheckErrorStatus(Statuses statuses, string errorStatus)
+        {
+            bool isError = false;
+            string errorMessage = string.Empty;
+
+            var status = statuses.Content.FirstOrDefault(s => s.Status.ToLower() == errorStatus);
+            if (status != null)
+            {
+                isError = true;
+                errorMessage = status.Description;
+            }
+
+            return (isError, errorMessage);
         }
     }
 }
