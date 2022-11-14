9352e3e002414e6805f716f3a5c3448e323b8f91
Ivar <ivar.nesje@finanstilsynet.no>
2022-11-14 07:58:52 +0100

    Improve code coverage

--- test/Altinn.App.Core.Tests/LayoutExpressions/FullTests/Test2/RunTest2.cs
+++ test/Altinn.App.Core.Tests/LayoutExpressions/FullTests/Test2/RunTest2.cs
@@ -21,23 +21,46 @@ public class RunTest2
     [Fact]
     public async Task RemoveWholeGroup()
     {
-        var state = await LayoutTestUtils.GetLayoutModelTools(
-            new DataModel()
+        var data = new DataModel()
+        {
+            Some = new()
             {
-                Some = new()
+                NotRepeating = "hideGroup",
+                Data = new()
                 {
-                    NotRepeating = "hideGroup",
-                    Data = new()
+                    new()
                     {
-                       new() { Binding = "binding" },
-                       new() { Binding2 = 2 },
-                    }
+                        Binding = default,
+                        Binding2 = default,
+                        Binding3 = default
+                    },
+                    new()
+                    {
+                        Binding = "binding",
+                        Binding2 = 2,
+                        Binding3 = default
+                    },
                 }
-            },
-            "Test2");
+            }
+        };
+        var state = await LayoutTestUtils.GetLayoutModelTools(data, "Test2");
         var hidden = LayoutEvaluator.GetHiddenFieldsForRemoval(state);
 
-        hidden.Should().BeEquivalentTo(new List<string> { "some.data[0].binding2", "some.data[1].binding2" });
+        // Should try to remove "some.data[0].binding2", because it is not nullable int and the parent object exists
+        hidden.Should().BeEquivalentTo(new List<string> { "some.data[0].binding2", "some.data[1].binding", "some.data[1].binding2" });
+
+        // Verify before removing data
+        data.Some.Data[0].Binding.Should().BeNull();
+        data.Some.Data[0].Binding2.Should().Be(0); // binding is not nullable, but will be reset to zero
+        data.Some.Data[1].Binding.Should().Be("binding");
+        data.Some.Data[1].Binding2.Should().Be(2);
+        LayoutEvaluator.RemoveHiddenData(state);
+
+        // Verify data was removed
+        data.Some.Data[0].Binding.Should().BeNull();
+        data.Some.Data[0].Binding2.Should().Be(0); // binding is not nullable, but will be reset to zero
+        data.Some.Data[1].Binding.Should().BeNull();
+        data.Some.Data[1].Binding2.Should().Be(0);
     }
 
     [Fact]
