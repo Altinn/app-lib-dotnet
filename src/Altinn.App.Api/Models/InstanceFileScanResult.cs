64e5d30f63bbb0e574691e79a38ea5944c4f62d2
Ronny Birkeli <ronny.birkeli@digdir.no>
2023-01-20 16:05:16 +0100

    Add light weight endpoint for status on file scan

---
+++ src/Altinn.App.Api/Models/InstanceFileScanResult.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Altinn.App.Core.Models;
+using Altinn.Platform.Storage.Interface.Enums;
+
+namespace Altinn.App.Api.Models
+{
+    /// <summary>
+    /// Light weight model representing an instance and it's file scan result status.
+    /// </summary>
+    public class InstanceFileScanResult
+    {
+        private readonly List<DataElementFileScanResult> _dataElements;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InstanceFileScanResult"/> class.
+        /// </summary>
+        public InstanceFileScanResult(InstanceIdentifier instanceIdentifier)
+        {
+            Id = instanceIdentifier;
+            _dataElements = new List<DataElementFileScanResult>();
+        }
+
+        /// <summary>
+        /// Instance id
+        /// </summary>
+        [JsonPropertyName("id")]
+        public InstanceIdentifier Id { get; set; }
+
+        /// <summary>
+        /// Contains the aggregated file scan result for an instance.
+        /// Infected = If any data elements has a status of Infected
+        /// Clean = If all data elements has status Clean
+        /// Pending = If all or some are Pending and the rest are Clean
+        /// </summary>
+        [JsonPropertyName("fileScanResult")]
+        public FileScanResult FileScanResult { get; private set; }
+
+        /// <summary>
+        /// File scan result for individual data elements.
+        /// </summary>
+        [JsonPropertyName("data")]
+        public IReadOnlyList<DataElementFileScanResult> DataElements => _dataElements.AsReadOnly();
+
+        /// <summary>
+        /// Adds a individual data element file scan result and updates the aggregated file scan result status
+        /// </summary>        
+        public void AddFileScanResult(DataElementFileScanResult dataElementFileScanResult)
+        {
+            _dataElements.Add(dataElementFileScanResult);
+
+            RecalculateAggregatedStatus();
+        }
+
+        private void RecalculateAggregatedStatus()
+        {
+            if (_dataElements.TrueForAll(dataElement => dataElement.FileScanResult == FileScanResult.Clean))
+            {
+                FileScanResult = FileScanResult.Clean;
+            }
+            else if (_dataElements.Any(dataElement => dataElement.FileScanResult == FileScanResult.Infected))
+            {
+                FileScanResult = FileScanResult.Infected;
+            }
+            else if (_dataElements.Any(dataElement => dataElement.FileScanResult == FileScanResult.Pending))
+            {
+                FileScanResult = FileScanResult.Pending;
+            }
+        }
+    }
+}
---
+++ test/Altinn.App.Api.Tests/Controllers/FileScanControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Altinn.App.Api.Controllers;
+using Altinn.App.Core.Interface;
+using Altinn.Platform.Storage.Interface.Models;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Altinn.App.Api.Tests.Controllers
+{
+    public class FileScanControllerTests
+    {
+        [Fact]
+        public async void AtLeasOneDataElementInfected()
+        {
+            const string org = "org";
+            const string app = "app";
+            const int instanceOwnerPartyId = 12345;
+            var instanceId = Guid.NewGuid();
+
+            Instance instance = new Instance
+            {
+                Id = instanceOwnerPartyId.ToString() + "/" + instanceId.ToString(),
+                Process = null,
+                Data = new List<DataElement>() 
+                {
+                    new() { Id = Guid.NewGuid().ToString(), FileScanResult = Platform.Storage.Interface.Enums.FileScanResult.Infected }
+                }
+            };
+
+
+            var instanceClientMock = new Mock<IInstance>();
+            instanceClientMock
+                .Setup(e => e.GetInstance(app, org, instanceOwnerPartyId, instanceId))
+                .Returns(Task.FromResult<Instance>(instance));
+
+            FileScanController fileScanController = new FileScanController(instanceClientMock.Object);
+
+            var fileScanResults = await fileScanController.GetFileScanResults(org, app, instanceOwnerPartyId, instanceId);
+
+            fileScanResults.Value?.FileScanResult.Should().Be(Platform.Storage.Interface.Enums.FileScanResult.Infected);
+        }
+    }
+}
