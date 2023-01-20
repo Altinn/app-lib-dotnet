70228026bb99b7dbe1254d9a2760af2e9772852e
Ronny Birkeli <ronny.birkeli@digdir.no>
2023-01-20 16:23:27 +0100

    Test cleanup

--- test/Altinn.App.Api.Tests/Controllers/FileScanControllerTests.cs
+++ test/Altinn.App.Api.Tests/Controllers/FileScanControllerTests.cs
@@ -2,11 +2,10 @@
 using Altinn.App.Core.Interface;
 using Altinn.Platform.Storage.Interface.Models;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -15,34 +14,54 @@ namespace Altinn.App.Api.Tests.Controllers
     public class FileScanControllerTests
     {
         [Fact]
-        public async void AtLeasOneDataElementInfected()
+        public async Task InstanceAndDataExists_ShouldReturn200Ok()
         {
             const string org = "org";
             const string app = "app";
             const int instanceOwnerPartyId = 12345;
-            var instanceId = Guid.NewGuid();
+            Guid instanceId = Guid.NewGuid();
+            Mock<IInstance> instanceClientMock = CreateInstanceClientMock(org, app, instanceOwnerPartyId, instanceId);
 
+            var fileScanController = new FileScanController(instanceClientMock.Object);
+            var fileScanResults = await fileScanController.GetFileScanResults(org, app, instanceOwnerPartyId, instanceId);
+
+            fileScanResults.Result.Should().BeOfType<OkObjectResult>();
+            fileScanResults.Value?.FileScanResult.Should().Be(Platform.Storage.Interface.Enums.FileScanResult.Infected);
+        }
+
+        [Fact]
+        public async Task InstanceDoesNotExists_ShouldReturnNotFound()
+        {
+            const string org = "org";
+            const string app = "app";
+            const int instanceOwnerPartyId = 12345;
+            Guid instanceId = Guid.NewGuid();
+            Mock<IInstance> instanceClientMock = CreateInstanceClientMock(org, app, instanceOwnerPartyId, instanceId);
+
+            var fileScanController = new FileScanController(instanceClientMock.Object);
+            var fileScanResults = await fileScanController.GetFileScanResults(org, app, instanceOwnerPartyId, Guid.NewGuid());
+
+            fileScanResults.Result.Should().BeOfType<NotFoundResult>();
+        }
+
+        private static Mock<IInstance> CreateInstanceClientMock(string org, string app, int instanceOwnerPartyId, Guid instanceId)
+        {
             Instance instance = new Instance
             {
                 Id = instanceOwnerPartyId.ToString() + "/" + instanceId.ToString(),
                 Process = null,
-                Data = new List<DataElement>() 
+                Data = new List<DataElement>()
                 {
                     new() { Id = Guid.NewGuid().ToString(), FileScanResult = Platform.Storage.Interface.Enums.FileScanResult.Infected }
                 }
             };
-
 
             var instanceClientMock = new Mock<IInstance>();
             instanceClientMock
                 .Setup(e => e.GetInstance(app, org, instanceOwnerPartyId, instanceId))
                 .Returns(Task.FromResult<Instance>(instance));
 
-            FileScanController fileScanController = new FileScanController(instanceClientMock.Object);
-
-            var fileScanResults = await fileScanController.GetFileScanResults(org, app, instanceOwnerPartyId, instanceId);
-
-            fileScanResults.Value?.FileScanResult.Should().Be(Platform.Storage.Interface.Enums.FileScanResult.Infected);
+            return instanceClientMock;
         }
     }
 }
