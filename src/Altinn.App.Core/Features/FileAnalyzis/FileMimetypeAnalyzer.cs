05cafeef568f6075df4842949837212b696b8a97
Ronny Birkeli <ronny.birkeli@digdir.no>
2023-03-15 22:28:05 +0100

    Started on tests

--- src/Altinn.App.Core/Features/FileAnalyzis/FileMimetypeAnalyzer.cs
+++ src/Altinn.App.Core/Features/FileAnalyzis/FileMimetypeAnalyzer.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Altinn.App.Core.Features.FileAnalyzis;
 
-namespace Altinn.App.Core.Features.FileAnalyzis
+/// <summary>
+/// Does a deep analysis of the filetype by scanning the binary
+/// for known string patterns and magic numbers.
+/// </summary>
+public class FileMimeTypeAnalyzer : IFileAnalyzer
 {
     /// <summary>
-    /// Does a deep analysis of the filetype by scanning the binary
-    /// for known string patterns and magic numbers.
+    /// Analyzes the content and returns any findings as name/value pairs.
     /// </summary>
-    public class FileMimeTypeAnalyzer : IFileAnalyzer
+    public IDictionary<string, string> Analyze(StreamContent streamContent)
     {
-        public IDictionary<string, string> Analyze(StreamContent streamContent)
-        {
-            Dictionary<string, string> metadata = new Dictionary<string, string>();
+        Dictionary<string, string> metadata = new Dictionary<string, string>();
 
-            return metadata;
-        }
+        return metadata;
     }
 }
---
+++ test/Altinn.App.Api.Tests/Controllers/DataControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using Altinn.App.Api.Tests.Utils;
+using Altinn.App.Core.Internal.Events;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Altinn.App.Api.Tests.Controllers
+{
+    public class DataControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        public DataControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task CreateDataElement_BinaryPdf_AnalyzerShouldRun()
+        {
+            var client = _factory.CreateClient();
+
+            string app = "contributer-restriction";
+            Guid guid = new Guid("0fc98a23-fe31-4ef5-8fb9-dd3f479354cd");
+            //TestDataUtil.DeleteInstance("tdd", app, 1337, guid);
+            //TestDataUtil.PrepareInstance("tdd", app, 1337, guid);
+            string token = PrincipalUtil.GetOrgToken("nav", "160694123");
+            string expectedMsg = "Invalid data provided. Error: The Content-Disposition header must contain a filename";
+
+            //HttpClient client = SetupUtil.GetTestClient(_factory, "tdd", app);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            string url = $"/tdd/{app}/instances/1337/{guid}/data?dataType=specificFileType";
+            HttpContent content = new StringContent(string.Empty);
+            content.Headers.ContentDisposition = ContentDispositionHeaderValue.Parse("attachment");
+
+            HttpResponseMessage response = await client.PostAsync(url, content);
+            string message = await response.Content.ReadAsStringAsync();
+            //TestDataUtil.DeleteInstanceAndData("tdd", app, 1337, guid);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal(expectedMsg, message);
+        }
+    }
+}
