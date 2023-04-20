1439c28fdb02c1ed421dc5ff2126a295a39b0338
Vemund Gaukstad <tjololo@users.noreply.github.com>
2023-04-20 11:08:21 +0200

    Consolidate test classes and replace JwtTokenUtil with UserTokenProvider

--- test/Altinn.App.Core.Tests/Infrastructure/Clients/DataClientTests.cs
+++ test/Altinn.App.Core.Tests/Infrastructure/Clients/DataClientTests.cs
@@ -1,152 +1,239 @@
 #nullable enable
 using System.Net;
-using System.Text.Json;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
 using Altinn.App.Core.Configuration;
 using Altinn.App.Core.Helpers;
 using Altinn.App.Core.Infrastructure.Clients.Storage;
 using Altinn.App.Core.Interface;
 using Altinn.App.Core.Models;
+using Altinn.App.PlatformServices.Tests.Data;
+using Altinn.App.PlatformServices.Tests.Mocks;
 using Altinn.Platform.Storage.Interface.Models;
 using FluentAssertions;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
-using Microsoft.Net.Http.Headers;
 using Moq;
 using Xunit;
 
-namespace Altinn.App.Core.Tests.Infrastructure.Clients;
-
-public class DataClientTests
+namespace Altinn.App.Core.Tests.Infrastructure.Clients
 {
-    private readonly string apiStorageEndpoint = "https://platform.altinn.no/api/storage/";
-    
-    [Fact]
-    public async Task UpdateBinaryData_put_updated_data_and_Return_DataElement()
+    public class DataClientTests
     {
-        Mock<HttpClient> httpClientMock;
-        var resultDataelement = new DataElement()
-        {
-            Id = "aaaa-bbbbb-cccc-dddd"
-        };
-        IData dataClient = GetDataClientWithMocks(
-            HttpStatusCode.Created,
-            resultDataelement,
-            out httpClientMock);
-        var instanceIdentifier = new InstanceIdentifier("501337/d3f3250d-705c-4683-a215-e05ebcbe6071");
-        var dataGuid = new Guid("67a5ef12-6e38-41f8-8b42-f91249ebcec0");
-        var restult = await dataClient.UpdateBinaryData(instanceIdentifier, "application/json", "test.json", dataGuid, new MemoryStream());
-        restult.Should().BeEquivalentTo(resultDataelement);
-        Uri expectedUri = new Uri($"{apiStorageEndpoint}instances/{instanceIdentifier}/data/{dataGuid}", UriKind.RelativeOrAbsolute);
-        httpClientMock.Verify(_ => _.SendAsync(
-            It.Is<HttpRequestMessage>(m => IsExpectedHttpRequest(m, expectedUri, "test.json", "application/json")), 
-            CancellationToken.None));
-        httpClientMock.VerifyNoOtherCalls();
-    }
-    
-    [Fact]
-    public async Task UpdateBinaryData_returns_exception_when_put_to_storage_result_in_servererror()
-    {
-        Mock<HttpClient> httpClientMock;
-        IData dataClient = GetDataClientWithMocks(
-            HttpStatusCode.InternalServerError,
-            null,
-            out httpClientMock);
-        var instanceIdentifier = new InstanceIdentifier("501337/d3f3250d-705c-4683-a215-e05ebcbe6071");
-        var dataGuid = new Guid("67a5ef12-6e38-41f8-8b42-f91249ebcec0");
-        await Assert.ThrowsAsync<PlatformHttpException>(async () => await dataClient.UpdateBinaryData(instanceIdentifier, "application/json", "test.json", dataGuid, new MemoryStream()));
-        Uri expectedUri = new Uri($"{apiStorageEndpoint}instances/{instanceIdentifier}/data/{dataGuid}", UriKind.RelativeOrAbsolute);
-        httpClientMock.Verify(_ => _.SendAsync(
-            It.Is<HttpRequestMessage>(m => IsExpectedHttpRequest(m, expectedUri, "test.json", "application/json")), 
-            CancellationToken.None));
-        httpClientMock.VerifyNoOtherCalls();
-    }
-    
-    [Fact]
-    public async Task UpdateBinaryData_returns_exception_when_put_to_storage_result_in_conflict()
-    {
-        Mock<HttpClient> httpClientMock;
-        IData dataClient = GetDataClientWithMocks(
-            HttpStatusCode.Conflict,
-            null,
-            out httpClientMock);
-        var instanceIdentifier = new InstanceIdentifier("501337/d3f3250d-705c-4683-a215-e05ebcbe6071");
-        var dataGuid = new Guid("67a5ef12-6e38-41f8-8b42-f91249ebcec0");
-        await Assert.ThrowsAsync<PlatformHttpException>(async () => await dataClient.UpdateBinaryData(instanceIdentifier, "application/json", "test.json", dataGuid, new MemoryStream()));
-        Uri expectedUri = new Uri($"{apiStorageEndpoint}instances/{instanceIdentifier}/data/{dataGuid}", UriKind.RelativeOrAbsolute);
-        httpClientMock.Verify(_ => _.SendAsync(
-            It.Is<HttpRequestMessage>(m => IsExpectedHttpRequest(m, expectedUri, "test.json", "application/json")), 
-            CancellationToken.None));
-        httpClientMock.VerifyNoOtherCalls();
-    }
+        private readonly Mock<IOptions<PlatformSettings>> platformSettingsOptions;
+        private readonly Mock<IUserTokenProvider> userTokenProvide;
+        private readonly ILogger<DataClient> logger;
+        private readonly string apiStorageEndpoint = "https://local.platform.altinn.no/api/storage/";
 
-    private IData GetDataClientWithMocks(HttpStatusCode resultStatusCode, DataElement? resultDataelement, out Mock<HttpClient> httpClientMock)
-    {
-        IOptions<PlatformSettings> platformSettings = Options.Create(new PlatformSettings()
+        public DataClientTests()
         {
-            ApiStorageEndpoint = apiStorageEndpoint
-        });
-        IOptionsMonitor<AppSettings> appSettings = new TestAppSettingsMonitor(new AppSettings()
-        {
-            RuntimeCookieName = "AltinnTestCookie"
-        });
-        Mock<IHttpContextAccessor> httpCtxAccessor = new Mock<IHttpContextAccessor>();
-        HttpContext httpCtx = new DefaultHttpContext();
-        httpCtx.Request.Headers["Authorization"] = "Bearer dummytesttoken";
-        httpCtxAccessor.Setup(_ => _.HttpContext).Returns(httpCtx);
-        httpClientMock = new Mock<HttpClient>();
-        HttpResponseMessage responseMessage = new HttpResponseMessage();
-        responseMessage.StatusCode = resultStatusCode;
-        if (resultDataelement != null)
-        {
-            responseMessage.Content = GetStreamContent(resultDataelement);
+            platformSettingsOptions = new Mock<IOptions<PlatformSettings>>();
+            PlatformSettings platformSettings = new() { ApiStorageEndpoint = apiStorageEndpoint };
+            platformSettingsOptions.Setup(s => s.Value).Returns(platformSettings);
+            userTokenProvide = new Mock<IUserTokenProvider>();
+            userTokenProvide.Setup(u => u.GetUserToken()).Returns("dummytesttoken");
+
+            logger = new NullLogger<DataClient>();
         }
 
-        httpClientMock.Setup(_ => _.SendAsync(It.IsAny<HttpRequestMessage>(), CancellationToken.None)).ReturnsAsync(responseMessage);
-        IData dataClient = new DataClient(platformSettings, NullLogger<DataClient>.Instance, httpCtxAccessor.Object, appSettings, httpClientMock.Object);
+        [Fact]
+        public async Task InsertBinaryData_MethodProduceValidPlatformRequest()
+        {
+            // Arrange
+            HttpRequestMessage platformRequest = null;
 
-        return dataClient;
+            var target = GetDataClient(async (HttpRequestMessage request, CancellationToken token) =>
+            {
+                platformRequest = request;
+
+                DataElement dataElement = new DataElement
+                {
+                    Id = "DataElement.Id",
+                    InstanceGuid = "InstanceGuid"
+                };
+                await Task.CompletedTask;
+                return new HttpResponseMessage() { Content = JsonContent.Create(dataElement) };
+            });
+
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes("This is not a pdf, but no one here will care."));
+            var instanceIdentifier = new InstanceIdentifier(323413, Guid.NewGuid());
+            Uri expectedUri = new Uri($"{apiStorageEndpoint}instances/{instanceIdentifier}/data?dataType=catstories", UriKind.RelativeOrAbsolute);
+
+            // Act
+            DataElement actual = await target.InsertBinaryData(instanceIdentifier.ToString(), "catstories", "application/pdf", "a cats story.pdf", stream);
+
+            // Assert
+            Assert.NotNull(actual);
+            
+            Assert.NotNull(platformRequest);
+            AssertHttpRequest(platformRequest, expectedUri, HttpMethod.Post, "\"a cats story.pdf\"", "application/pdf");
+        }
+
+        [Fact]
+        public async Task GetFormData_MethodProduceValidPlatformRequest_ReturnedFormIsValid()
+        {
+            // Arrange
+            HttpRequestMessage platformRequest = null;
+
+            var target = GetDataClient(async (HttpRequestMessage request, CancellationToken token) =>
+            {
+                platformRequest = request;
+
+                string serializedModel = string.Empty
+                                         + @"<?xml version=""1.0""?>"
+                                         + @"<Skjema xmlns=""urn:no:altinn:skjema:v1"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"""
+                                         + @"  skjemanummer=""1472"" spesifikasjonsnummer=""9812"" blankettnummer=""AFP-01"" tittel=""Arbeidsgiverskjema AFP"" gruppeid=""8818"">"
+                                         + @"  <Foretak-grp-8820 gruppeid=""8820"">"
+                                         + @"    <EnhetNavnEndring-datadef-31 orid=""31"">Test Test 123</EnhetNavnEndring-datadef-31>"
+                                         + @"  </Foretak-grp-8820>"
+                                         + @"</Skjema>";
+                await Task.CompletedTask;
+
+                HttpResponseMessage response = new HttpResponseMessage() { Content = new StringContent(serializedModel) };
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/xml");
+                return response;
+            });
+
+            Guid dataElementGuid = Guid.NewGuid();
+            var instanceIdentifier = new InstanceIdentifier(323413, Guid.NewGuid());
+            Uri expectedUri = new Uri($"{apiStorageEndpoint}instances/{instanceIdentifier}/data/{dataElementGuid}", UriKind.RelativeOrAbsolute);
+
+            // Act
+            object response = await target.GetFormData(instanceIdentifier.InstanceGuid, typeof(SkjemaWithNamespace), "org", "app", 323413, dataElementGuid);
+
+            // Assert
+            var actual = response as SkjemaWithNamespace; 
+            Assert.NotNull(actual);
+            Assert.NotNull(actual.Foretakgrp8820);
+            Assert.NotNull(actual.Foretakgrp8820.EnhetNavnEndringdatadef31);
+
+            Assert.NotNull(platformRequest);
+            AssertHttpRequest(platformRequest, expectedUri, HttpMethod.Get, null, "application/xml");
+        }
+
+        [Fact]
+        public async Task InsertBinaryData_PlatformRespondNotOk_ThrowsPlatformException()
+        {
+            // Arrange
+            HttpRequestMessage platformRequest = null;
+            var target = GetDataClient(async (HttpRequestMessage request, CancellationToken token) =>
+            {
+                platformRequest = request;
+
+                await Task.CompletedTask;
+                return new HttpResponseMessage() { StatusCode = HttpStatusCode.BadRequest };
+            });
+
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes("This is not a pdf, but no one here will care."));
+
+            // Act
+            var actual = await Assert.ThrowsAsync<PlatformHttpException>(async () => await target.InsertBinaryData("instanceId", "catstories", "application/pdf", "a cats story.pdf", stream));
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(HttpStatusCode.BadRequest, actual.Response.StatusCode);
+        }
+        
+        [Fact]
+        public async Task UpdateBinaryData_put_updated_data_and_Return_DataElement()
+        {
+            var instanceIdentifier = new InstanceIdentifier("501337/d3f3250d-705c-4683-a215-e05ebcbe6071");
+            var dataGuid = new Guid("67a5ef12-6e38-41f8-8b42-f91249ebcec0");
+            HttpRequestMessage platformRequest = null;
+            int invokations = 0;
+            DataElement expectedDataelement = new DataElement
+            {
+                Id = instanceIdentifier.ToString(),
+                InstanceGuid = instanceIdentifier.InstanceGuid.ToString()
+            };
+            var dataClient = GetDataClient(async (HttpRequestMessage request, CancellationToken token) =>
+            {
+                invokations++;
+                platformRequest = request;
+                
+                DataElement dataElement = new DataElement
+                {
+                    Id = instanceIdentifier.ToString(),
+                    InstanceGuid = instanceIdentifier.InstanceGuid.ToString()
+                };
+                await Task.CompletedTask;
+                return new HttpResponseMessage() { Content = JsonContent.Create(dataElement) };
+            });
+            Uri expectedUri = new Uri($"{apiStorageEndpoint}instances/{instanceIdentifier}/data/{dataGuid}", UriKind.RelativeOrAbsolute);
+            var restult = await dataClient.UpdateBinaryData(instanceIdentifier, "application/json", "test.json", dataGuid, new MemoryStream());
+            invokations.Should().Be(1);
+            AssertHttpRequest(platformRequest, expectedUri, HttpMethod.Put, "test.json", "application/json");
+            restult.Should().BeEquivalentTo(expectedDataelement);
+        }
+
+        [Fact]
+        public async Task UpdateBinaryData_returns_exception_when_put_to_storage_result_in_servererror()
+        {
+            var instanceIdentifier = new InstanceIdentifier("501337/d3f3250d-705c-4683-a215-e05ebcbe6071");
+            var dataGuid = new Guid("67a5ef12-6e38-41f8-8b42-f91249ebcec0");
+            int invokations = 0;
+            var dataClient = GetDataClient(async (HttpRequestMessage request, CancellationToken token) =>
+            {
+                invokations++;
+                
+                await Task.CompletedTask;
+                return new HttpResponseMessage() { StatusCode = HttpStatusCode.InternalServerError };
+            });
+            var actual = await Assert.ThrowsAsync<PlatformHttpException>(async () => await dataClient.UpdateBinaryData(instanceIdentifier, "application/json", "test.json", dataGuid, new MemoryStream()));
+            actual.Should().NotBeNull();
+            actual.Response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+        
+        [Fact]
+        public async Task UpdateBinaryData_returns_exception_when_put_to_storage_result_in_conflict()
+        {
+            var instanceIdentifier = new InstanceIdentifier("501337/d3f3250d-705c-4683-a215-e05ebcbe6071");
+            var dataGuid = new Guid("67a5ef12-6e38-41f8-8b42-f91249ebcec0");
+            int invokations = 0;
+            var dataClient = GetDataClient(async (HttpRequestMessage request, CancellationToken token) =>
+            {
+                invokations++;
+                
+                await Task.CompletedTask;
+                return new HttpResponseMessage() { StatusCode = HttpStatusCode.Conflict };
+            });
+            var actual = await Assert.ThrowsAsync<PlatformHttpException>(async () => await dataClient.UpdateBinaryData(instanceIdentifier, "application/json", "test.json", dataGuid, new MemoryStream()));
+            actual.Should().NotBeNull();
+            actual.Response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+        }
+
+        private DataClient GetDataClient(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handlerFunc)
+        {
+            DelegatingHandlerStub dhStub = new(handlerFunc);
+            return new DataClient(
+                platformSettingsOptions.Object,
+                logger,
+                new HttpClient(dhStub),
+                userTokenProvide.Object);
+        }
+
+        private void AssertHttpRequest(HttpRequestMessage actual, Uri expectedUri, HttpMethod method, string? expectedFilename, string? expectedContentType)
+        {
+            IEnumerable<string>? actualContentType = null;
+            IEnumerable<string>? actualContentDisposition = null;
+            actual.Content?.Headers.TryGetValues("Content-Type", out actualContentType);
+            actual.Content?.Headers.TryGetValues("Content-Disposition", out actualContentDisposition);
+            var authHeader = actual.Headers.Authorization;
+            actual.RequestUri.Should().BeEquivalentTo(expectedUri);
+            Uri.Compare(actual.RequestUri, expectedUri, UriComponents.HttpRequestUrl, UriFormat.SafeUnescaped, StringComparison.OrdinalIgnoreCase).Should().Be(0, "Actual request Uri did not match expected Uri");
+            if (expectedContentType is not null)
+            {
+                actualContentType?.FirstOrDefault().Should().BeEquivalentTo(expectedContentType);
+            }
+            
+            if (expectedFilename is not null)
+            {
+                ContentDispositionHeaderValue.Parse(actualContentDisposition?.FirstOrDefault()).FileName?.Should().BeEquivalentTo(expectedFilename);
+            }
+
+            authHeader.Parameter.Should().BeEquivalentTo("dummytesttoken");
+        }
     }
-
-    private static StreamContent GetStreamContent(DataElement dataElement)
-    {
-        string de = JsonSerializer.Serialize(dataElement);
-        var stream = new MemoryStream();
-        var writer = new StreamWriter(stream);
-        writer.Write(de);
-        writer.Flush();
-        stream.Position = 0;
-        return new StreamContent(stream);
-    }
-
-    private bool IsExpectedHttpRequest(HttpRequestMessage actual, Uri expectedUri, string expectedFilename, string expectedContentType)
-    {
-        IEnumerable<string>? actualContentType = null;
-        IEnumerable<string>? actualContentDisposition = null;
-        var contentTypeSet = actual.Content?.Headers.TryGetValues("Content-Type", out actualContentType);
-        var contentDispositionSet = actual.Content?.Headers.TryGetValues("Content-Disposition", out actualContentDisposition);
-        return Uri.Compare(actual.RequestUri, expectedUri, UriComponents.AbsoluteUri, UriFormat.SafeUnescaped, StringComparison.OrdinalIgnoreCase) == 0
-               && contentTypeSet is true && actualContentType!.FirstOrDefault()!.Equals(expectedContentType)
-               && contentDispositionSet is true && ContentDispositionHeaderValue.Parse(actualContentDisposition!.FirstOrDefault()).FileName.Equals(expectedFilename);
-    }
-}
-
-internal class TestAppSettingsMonitor : IOptionsMonitor<AppSettings>
-{
-    public TestAppSettingsMonitor(AppSettings appSettings)
-    {
-        CurrentValue = appSettings;
-    }
-
-    public AppSettings Get(string name)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IDisposable OnChange(Action<AppSettings, string> listener)
-    {
-        throw new NotImplementedException();
-    }
-
-    public AppSettings CurrentValue { get; }
 }
