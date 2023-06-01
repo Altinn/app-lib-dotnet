892f09cb9d1b220da4b64cf90faa7defce0398db
Ronny Birkeli <ronny.birkeli@digdir.no>
2023-06-01 12:26:29 +0200

    Set ValidationIssue properties to nullable.

--- src/Altinn.App.Core/Models/Validation/ValidationIssue.cs
+++ src/Altinn.App.Core/Models/Validation/ValidationIssue.cs
@@ -19,30 +19,30 @@ namespace Altinn.App.Core.Models.Validation
         /// The unique id of the specific element with the identified issue.
         /// </summary>
         [JsonProperty(PropertyName = "instanceId")]
-        public string InstanceId { get; set; }
+        public string? InstanceId { get; set; }
 
         /// <summary>
         /// The uniqe id of the data element of a given instance with the identified issue.
         /// </summary>
         [JsonProperty(PropertyName = "dataElementId")]
-        public string DataElementId { get; set; }
+        public string? DataElementId { get; set; }
 
         /// <summary>
         /// A reference to a property the issue is a bout.
         /// </summary>
         [JsonProperty(PropertyName = "field")]
-        public string Field { get; set; }
+        public string? Field { get; set; }
 
         /// <summary>
         /// A system readable identification of the type of issue.
         /// </summary>
         [JsonProperty(PropertyName = "code")]
-        public string Code { get; set; }
+        public string? Code { get; set; }
 
         /// <summary>
         /// A human readable description of the issue.
         /// </summary>
         [JsonProperty(PropertyName = "description")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
     }
 }
