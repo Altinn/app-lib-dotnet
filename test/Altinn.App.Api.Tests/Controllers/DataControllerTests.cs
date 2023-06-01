ba1456f804ff19d42a504969bdcc5f349743d9d9
Ronny Birkeli <ronny.birkeli@digdir.no>
2023-06-01 12:04:11 +0200

    Cleanup

--- test/Altinn.App.Api.Tests/Controllers/DataControllerTests.cs
+++ test/Altinn.App.Api.Tests/Controllers/DataControllerTests.cs
@@ -45,7 +45,6 @@ namespace Altinn.App.Api.Tests.Controllers
 
             // This is where it happens
             HttpResponseMessage response = await client.SendAsync(request);
-            request.Dispose();
 
             // Cleanup testdata
             TestData.DeleteInstanceAndData(org, app, 1337, guid);
@@ -78,7 +77,6 @@ namespace Altinn.App.Api.Tests.Controllers
             // This is where it happens
             HttpResponseMessage response = await client.SendAsync(request);
             string responseContent = await response.Content.ReadAsStringAsync();
-            request.Dispose();
 
             // Cleanup testdata
             TestData.DeleteInstanceAndData(org, app, 1337, guid);
