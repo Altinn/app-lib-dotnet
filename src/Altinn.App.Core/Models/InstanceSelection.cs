8d7eab4279019d71f8e1e9f65a893638b5ea8d74
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2023-07-10 15:50:32 +0200

    remove pagination class

--- src/Altinn.App.Core/Models/InstanceSelection.cs
+++ src/Altinn.App.Core/Models/InstanceSelection.cs
@@ -8,15 +8,21 @@ namespace Altinn.App.Core.Models
     public class InstanceSelection
     {
         /// <summary>
+        /// A list of selectable options for amount of rows per page to show for pagination
+        /// </summary>
+        [JsonProperty(PropertyName = "rowsPerPageOptions")]
+        public List<int>? RowsPerPageOptions { get; set; }
+
+        /// <summary>
+        /// The default amount of rows per page to show for pagination
+        /// </summary>
+        [JsonProperty(PropertyName = "defaultRowsPerPage")]
+        public int? DefaultRowsPerPage { get; set; }
+
+        /// <summary>
         /// The direction of sorting the list of instances, asc or desc
         /// </summary>
         [JsonProperty(PropertyName = "sortDirection")]
         public string? SortDirection { get; set; }
-
-        /// <summary>
-        /// Settings for pagination
-        /// </summary>
-        [JsonProperty(PropertyName = "pagination")]
-        public Pagination? Pagination { get; set; } 
     }
 }
