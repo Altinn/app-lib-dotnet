376702f57a74de405c2b4c2fffc76fb05ea61f86
Vemund Gaukstad <tjololo@users.noreply.github.com>
2023-09-19 10:59:52 +0200

    Fix tests after merge

--- test/Altinn.App.Core.Tests/LayoutExpressions/FullTests/Test3/RunTest3.cs
+++ test/Altinn.App.Core.Tests/LayoutExpressions/FullTests/Test3/RunTest3.cs
@@ -16,7 +16,7 @@ public class RunTest3
     }
 
     [Fact]
-    public async Task RemoveRowFromGroup()
+    public async Task RemoveRowDataFromGroup()
     {
         var data = new DataModel()
         {
@@ -63,7 +63,64 @@ public class RunTest3
         data.Some.Data[2].Binding3.Should().Be("text");
         LayoutEvaluator.RemoveHiddenData(state);
 
-        // Verify data was removed
+        // Verify row not deleted but fields null
+        data.Some.Data.Should().HaveCount(3);
+        data.Some.Data[0].Binding.Should().BeNull();
+        data.Some.Data[0].Binding2.Should().Be(0); // binding is not nullable, but will be reset to zero
+        data.Some.Data[1].Binding.Should().Be("binding");
+        data.Some.Data[1].Binding2.Should().Be(2);
+        data.Some.Data[2].Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task RemoveRowFromGroup()
+    {
+        var data = new DataModel()
+        {
+            Some = new()
+            {
+                NotRepeating = "showGroup",
+                Data = new()
+                {
+                    new()
+                    {
+                        Binding = default,
+                        Binding2 = default,
+                        Binding3 = default
+                    },
+                    new()
+                    {
+                        Binding = "binding",
+                        Binding2 = 2,
+                        Binding3 = default
+                    },
+                    new()
+                    {
+                        Binding = "hideRow",
+                        Binding2 = 3,
+                        Binding3 = "text"
+                    }
+                }
+            }
+        };
+        var state = await LayoutTestUtils.GetLayoutModelTools(data, "Test3");
+        var hidden = LayoutEvaluator.GetHiddenFieldsForRemoval(state);
+
+        // Should try to remove "some.data[0].binding2", because it is not nullable int and the parent object exists
+        hidden.Should().BeEquivalentTo(new List<string> { "some.data[2]" });
+
+        // Verify before removing data
+        data.Some.Data.Should().HaveCount(3);
+        data.Some.Data[0].Binding.Should().BeNull();
+        data.Some.Data[0].Binding2.Should().Be(0); // binding is not nullable, but will be reset to zero
+        data.Some.Data[1].Binding.Should().Be("binding");
+        data.Some.Data[1].Binding2.Should().Be(2);
+        data.Some.Data[2].Binding.Should().Be("hideRow");
+        data.Some.Data[2].Binding2.Should().Be(3);
+        data.Some.Data[2].Binding3.Should().Be("text");
+        
+        // Verify rows deleted
+        LayoutEvaluator.RemoveHiddenData(state, true);
         data.Some.Data.Should().HaveCount(2);
         data.Some.Data[0].Binding.Should().BeNull();
         data.Some.Data[0].Binding2.Should().Be(0); // binding is not nullable, but will be reset to zero
