64a9c214b01664ebd1e638d67a9f7b845b378c1d
HauklandJ <johannes.haukland@digdir.no>
2024-03-12 13:13:02 +0100

    feat: add models for email notification

---
+++ src/Altinn.App.Core/Models/Email/EmailNotification.cs
@@ -0,0 +1,60 @@
+﻿using Altinn.App.Core.Infrastructure.Clients.Notification;
+
+namespace Altinn.App.Core.Models.Email;
+/// <summary>
+/// Structure used by <see cref="EmailNotificationClient"/> to request an email notification to a list of recipients.
+/// </summary>
+public class EmailNotification
+{
+    /// <summary>
+    /// The subject of the email.
+    /// </summary>
+    public string Subject { get; set; }
+    /// <summary>
+    /// The body of the email. 
+    /// </summary>
+    public string Body { get; set; }
+    /// <summary>
+    /// The content type of the email. 
+    /// "Plain" by default.
+    /// </summary>
+    public string ContentType { get; set; }
+    /// <summary>
+    /// The Requested send time for the email. 
+    /// DateTime.Now by default.
+    /// </summary>
+    public DateTime RequestedSendTime { get; set; }
+    /// <summary>
+    /// The senders reference for the email. 
+    /// Used to track the email request.
+    /// </summary>
+    public string SendersReference { get; set; }
+    /// <summary>
+    /// The recipients of the email. 
+    /// </summary>
+    public List<EmailRecipient> Recipients { get; set; } = [];
+    /// <summary>
+    /// Structure used by <see cref="EmailNotificationClient"/> to request an email notification to a list of recipients.
+    /// </summary>
+    /// <param name="subject"><inheritdoc cref="Subject"/></param>
+    /// <param name="body"><inheritdoc cref="Body"/></param>
+    /// <param name="emailRecipients"><inheritdoc cref="Recipients"/></param>
+    /// <param name="sendersReference"><inheritdoc cref="SendersReference"/></param>
+    /// <param name="contentType"><inheritdoc cref="ContentType"/></param>
+    /// <param name="requestedSendTime"><inheritdoc cref="RequestedSendTime"/></param>
+    public EmailNotification(
+        string subject, 
+        string body, 
+        List<EmailRecipient> emailRecipients, 
+        string sendersReference, 
+        string contentType = "Plain", 
+        DateTime? requestedSendTime = null)
+    {
+        Subject = subject;
+        Body = body;
+        Recipients = emailRecipients;
+        SendersReference = sendersReference;
+        ContentType = contentType;
+        RequestedSendTime = requestedSendTime is null ? DateTime.Now : (DateTime) requestedSendTime;
+    }
+}
---
+++ src/Altinn.App.Core/Models/Email/EmailRecipient.cs
@@ -0,0 +1,28 @@
+﻿namespace Altinn.App.Core.Models.Email;
+/// <summary>
+/// Represents the recipient of an email.
+/// </summary>
+public class EmailRecipient
+{
+    /// <summary>
+    /// The email of the recipient.
+    /// </summary>
+    public string EmailAddress { get; set; }
+    /// <summary>
+    /// The mobile number of the recipient.
+    /// </summary>
+    public string? MobileNumber { get; set; } // TODO: remove?
+    /// <summary>
+    /// The organisation number of the recipient.
+    /// </summary>
+    public string? OrganisationNumber { get; set; } // TODO: remove?
+    /// <summary>
+    /// The national identifier (NIN) of the recipient.
+    /// </summary>
+    public string? NationalIdentityNumber { get; set; } // TODO: remove?
+    /// <summary>
+    /// The recipient of an email.
+    /// </summary>
+    /// <param name="emailAddress">The email address of the recipient. Required.</param>
+    public EmailRecipient(string emailAddress) => EmailAddress = emailAddress;
+}
