8125c71a75f8854ad815a90be8e3ec33f146211c
HauklandJ <johannes.haukland@digdir.no>
2024-03-12 14:29:51 +0100

    feat: add EmailNotificationException

---
+++ src/Altinn.App.Core/Internal/Email/EmailNotificationException.cs
@@ -0,0 +1,32 @@
+﻿namespace Altinn.App.Core.Internal.Email;
+
+/// <summary>
+/// Class representing an exception throw when an email notification could not be sent.
+/// </summary>
+public class EmailNotificationException : Exception
+{
+    /// <summary>
+    /// Creates a new Exception of <see cref="EmailNotificationException"/>
+    /// Intended to be used when the email notification fails.
+    /// </summary>
+    public EmailNotificationException()
+    {
+    }
+
+    /// <summary>
+    /// Creates a new Exception of <see cref="EmailNotificationException"/>
+    /// Intended to be used when the email notification fails.
+    /// </summary>
+    public EmailNotificationException(string? message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Creates a new Exception of <see cref="EmailNotificationException"/>
+    /// Intended to be used when the email notification fails.
+    /// </summary>
+    public EmailNotificationException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
+
