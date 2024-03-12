e828d21bf4b73f6c23f3af96a0207fcf39fa19c8
HauklandJ <johannes.haukland@digdir.no>
2024-03-12 12:27:31 +0100

    feat: add scoffolding for EmailNotificationClient and related interface

---
+++ src/Altinn.App.Core/Infrastructure/Clients/Notification/EmailNotificationClient.cs
@@ -0,0 +1,11 @@
+﻿using Altinn.App.Core.Internal.Email;
+
+namespace Altinn.App.Core.Infrastructure.Clients.Notification;
+/// <summary>
+/// Implementation of the <see cref="IEmailNotificationClient"/> interface using a HttpClient to send
+/// requests to the Email Notification service.
+/// </summary>
+public class EmailNotificationClient : IEmailNotificationClient
+{
+
+}
---
+++ src/Altinn.App.Core/Internal/Email/IEmailNotificationClient.cs
@@ -0,0 +1,12 @@
+namespace Altinn.App.Core.Internal.Email;
+
+/// <summary>
+/// Defines the required operations on a client of the PDF generator service.
+/// </summary>
+public interface IEmailNotificationClient
+{
+    /// <summary>
+    /// Sends an email.
+    /// </summary>
+    /// <returns>TODO: fill in returns</returns>
+}
