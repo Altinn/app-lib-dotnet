104d7ac31778d52d525cf823e2155f2111b17e2e
HauklandJ <johannes.haukland@digdir.no>
2024-03-13 12:39:50 +0100

    refactor: make email value objects sealed

--- src/Altinn.App.Core/Models/Email/EmailNotification.cs
+++ src/Altinn.App.Core/Models/Email/EmailNotification.cs
@@ -4,35 +4,35 @@ namespace Altinn.App.Core.Models.Email;
 /// <summary>
 /// Structure used by <see cref="EmailNotificationClient"/> to request an email notification to a list of recipients.
 /// </summary>
-public class EmailNotification
+public sealed class EmailNotification
 {
     /// <summary>
     /// The subject of the email.
     /// </summary>
-    public string Subject { get; set; }
+    public string Subject { get; init; }
     /// <summary>
     /// The body of the email. 
     /// </summary>
-    public string Body { get; set; }
+    public string Body { get; init; }
     /// <summary>
     /// The content type of the email. 
     /// "Plain" by default.
     /// </summary>
-    public string ContentType { get; set; }
+    public string ContentType { get; init; }
     /// <summary>
     /// The Requested send time for the email. 
     /// DateTime.Now by default.
     /// </summary>
-    public DateTime RequestedSendTime { get; set; }
+    public DateTime RequestedSendTime { get; init; }
     /// <summary>
     /// The senders reference for the email. 
     /// Used to track the email request.
     /// </summary>
-    public string SendersReference { get; set; }
+    public string SendersReference { get; init; }
     /// <summary>
     /// The recipients of the email. 
     /// </summary>
-    public List<EmailRecipient> Recipients { get; set; } = [];
+    public IReadOnlyList<EmailRecipient> Recipients { get; init; }
     /// <summary>
     /// Structure used by <see cref="EmailNotificationClient"/> to request an email notification to a list of recipients.
     /// </summary>
@@ -43,11 +43,11 @@ public class EmailNotification
     /// <param name="contentType"><inheritdoc cref="ContentType"/></param>
     /// <param name="requestedSendTime"><inheritdoc cref="RequestedSendTime"/></param>
     public EmailNotification(
-        string subject, 
-        string body, 
-        List<EmailRecipient> emailRecipients, 
-        string sendersReference, 
-        string contentType = "Plain", 
+        string subject,
+        string body,
+        List<EmailRecipient> emailRecipients,
+        string sendersReference,
+        string contentType = "Plain",
         DateTime? requestedSendTime = null)
     {
         Subject = subject;
@@ -55,6 +55,6 @@ public class EmailNotification
         Recipients = emailRecipients;
         SendersReference = sendersReference;
         ContentType = contentType;
-        RequestedSendTime = requestedSendTime is null ? DateTime.Now : (DateTime) requestedSendTime;
+        RequestedSendTime = requestedSendTime is null ? DateTime.Now : (DateTime)requestedSendTime;
     }
 }
