9ce90b077a82a0d9254c9040087fc3533e3632dc
Ronny Birkeli <ronny.birkeli@digdir.no>
2024-03-13 17:26:00 +0100

    Added test of ExchangeToAltinnToken

--- test/Altinn.App.Core.Tests/Internal/Maskinporten/MaskinportenJwkTokenProviderTests.cs
+++ test/Altinn.App.Core.Tests/Internal/Maskinporten/MaskinportenJwkTokenProviderTests.cs
@@ -5,6 +5,7 @@ using Altinn.App.Core.Internal.Maskinporten;
 using Altinn.App.Core.Internal.Secrets;
 using FluentAssertions;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Moq;
 using Xunit;
 
@@ -15,15 +16,16 @@ public class MaskinportenJwkTokenProviderTests
     [Fact]
     public async Task GetToken_ShouldReturnToken()
     {
-        var maskinportenService = new Mock<IMaskinportenService>();
-        maskinportenService.Setup(s => s.GetToken(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>()))
-            .ReturnsAsync(new TokenResponse { AccessToken = "myMaskinportenAccessToken" });
-
         IOptions<MaskinportenSettings> maskinportenSettings = Options.Create(new MaskinportenSettings()
         {
             Environment = "ver2",
             ClientId = Guid.NewGuid().ToString()
         });
+
+        var maskinportenService = new Mock<IMaskinportenService>();
+        maskinportenService.Setup(s => s.GetToken(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>()))
+            .ReturnsAsync(new TokenResponse { AccessToken = "myMaskinportenAccessToken" });
+        
         var secretsClient = new Mock<ISecretsClient>();
         secretsClient.Setup(s => s.GetSecretAsync(It.IsAny<string>())).ReturnsAsync("myBase64EncodedJwk");
 
@@ -32,7 +34,38 @@ public class MaskinportenJwkTokenProviderTests
         string scopes = "altinn:serviceowner/instances.read";
         string token = await maskinportenJwkTokenProvider.GetToken(scopes);
 
-        token.Should().NotBeNullOrEmpty();
+        token.Should().Be("myMaskinportenAccessToken");
+        maskinportenService.Verify(s => s.GetToken("myBase64EncodedJwk", maskinportenSettings.Value.Environment, maskinportenSettings.Value.ClientId, scopes, string.Empty, null, false));
+        secretsClient.Verify(s => s.GetSecretAsync("nameOfMySecretInKeyVault"));
+    }
+
+    [Fact]
+    public async Task GetAltinnExchangedToken_ShouldReturnToken()
+    {
+        IOptions<MaskinportenSettings> maskinportenSettings = Options.Create(new MaskinportenSettings()
+        {
+            Environment = "ver2",
+            ClientId = Guid.NewGuid().ToString()
+        });
+
+        TokenResponse maskinportenTokenResponse = new() { AccessToken = "myMaskinportenAccessToken" };
+        var maskinportenService = new Mock<IMaskinportenService>();
+        maskinportenService.Setup(s => s.GetToken(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>()))
+            .ReturnsAsync(maskinportenTokenResponse);
+
+        TokenResponse altinnTokenResponse = new() { AccessToken = "myAltinnAccessToken" };
+        maskinportenService.Setup(s => s.ExchangeToAltinnToken(maskinportenTokenResponse, maskinportenSettings.Value.Environment, null, null, false, false))
+            .ReturnsAsync(altinnTokenResponse);
+        
+        var secretsClient = new Mock<ISecretsClient>();
+        secretsClient.Setup(s => s.GetSecretAsync(It.IsAny<string>())).ReturnsAsync("myBase64EncodedJwk");
+
+        MaskinportenJwkTokenProvider maskinportenJwkTokenProvider = new(maskinportenService.Object, maskinportenSettings, secretsClient.Object, "nameOfMySecretInKeyVault");
+
+        string scopes = "altinn:serviceowner/instances.read";
+        string token = await maskinportenJwkTokenProvider.GetAltinnExchangedToken(scopes);
+
+        token.Should().Be("myAltinnAccessToken");
         maskinportenService.Verify(s => s.GetToken("myBase64EncodedJwk", maskinportenSettings.Value.Environment, maskinportenSettings.Value.ClientId, scopes, string.Empty, null, false));
         secretsClient.Verify(s => s.GetSecretAsync("nameOfMySecretInKeyVault"));
     }
