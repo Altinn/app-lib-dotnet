c1dc8c5fa0acb8a528058341ef1b22ccc30c06a8
HauklandJ <johannes.haukland@digdir.no>
2024-03-13 16:28:39 +0100

    feat: update EmailOrderResponse to serialize camelcase

--- src/Altinn.App.Core/Models/Email/EmailOrderResponse.cs
+++ src/Altinn.App.Core/Models/Email/EmailOrderResponse.cs
@@ -1,6 +1,23 @@
-﻿namespace Altinn.App.Core.Models.Email;
+﻿using System.Text.Json.Serialization;
+
+namespace Altinn.App.Core.Models.Email;
+
 /// <summary>
-/// 
+/// The response from the email notification API.
 /// </summary>
-/// <param name="OrderId">The id of the order.</param>
-public record EmailOrderResponse(string OrderId);
+public record EmailOrderResponse
+{
+    /// <summary>
+    /// The id of the email order.
+    /// </summary>
+    [JsonPropertyName("orderId")]
+    public string OrderId { get; init; }
+    /// <summary>
+    /// Initializes the EmailOrderResponse class.
+    /// </summary>
+    /// <param name="orderId"><inheritdoc cref="OrderId"/>></param>
+    public EmailOrderResponse(string orderId)
+    {
+        OrderId = orderId;
+    }
+}
