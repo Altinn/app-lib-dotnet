7a3a4d1e790778fbafaf8b356f96cf2a5390b985
HauklandJ <johannes.haukland@digdir.no>
2024-03-13 12:40:54 +0100

    feat: add response value object for email order

---
+++ src/Altinn.App.Core/Models/Email/EmailOrderResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Altinn.App.Core.Models.Email;
+/// <summary>
+/// 
+/// </summary>
+/// <param name="OrderId">The id of the order.</param>
+public record EmailOrderResponse(string OrderId);
