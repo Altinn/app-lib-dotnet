f8b8bef3d50ad127a7c1456a79d0cd70e825f811
HauklandJ <johannes.haukland@digdir.no>
2024-03-14 10:24:55 +0100

    feat: metrics and telemetry for email client

--- src/Altinn.App.Core/Infrastructure/Clients/Email/EmailNotificationClient.cs
+++ src/Altinn.App.Core/Infrastructure/Clients/Email/EmailNotificationClient.cs
@@ -4,7 +4,10 @@ using Altinn.App.Core.Internal.Email;
 using Altinn.App.Core.Models;
 using Altinn.App.Core.Models.Email;
 using Altinn.Common.AccessTokenClient.Services;
+using Microsoft.ApplicationInsights;
 using Microsoft.Extensions.Options;
+using Prometheus;
+using System.Diagnostics;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
@@ -20,6 +23,10 @@ public class EmailNotificationClient : IEmailNotificationClient
     private readonly IAppMetadata _appMetadata;
     private readonly PlatformSettings _platformSettings;
     private readonly IAccessTokenGenerator _accessTokenGenerator;
+    private readonly TelemetryClient _telemetryClient;
+    private static readonly Counter _orderCount = Metrics
+        .CreateCounter("altinn_app_notification_order_request_count", "Number of notification order requests.", labelNames: ["email"]);
+
     private readonly JsonSerializerOptions _jsonSerializerOptions = new()
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase
@@ -29,8 +36,12 @@ public class EmailNotificationClient : IEmailNotificationClient
     /// <exception cref="EmailNotificationException"/>
     public async Task<EmailOrderResponse> Order(EmailNotification emailNotification, CancellationToken ct)
     {
+        var startTime = DateTime.UtcNow;
+        var timer = Stopwatch.StartNew();
+
         HttpResponseMessage? httpResponseMessage = null;
         string? httpContent = null;
+        EmailOrderResponse? orderResponse = null;
         try
         {
             var requestContent = JsonSerializer.Serialize(emailNotification, _jsonSerializerOptions);
@@ -48,6 +59,7 @@ public class EmailNotificationClient : IEmailNotificationClient
             httpResponseMessage = await _httpClient.SendAsync(httpRequest, ct);
             if (!httpResponseMessage.IsSuccessStatusCode)
             {
+                _orderCount.WithLabels("error").Inc();
                 httpContent = await httpResponseMessage.Content.ReadAsStringAsync(ct);
                 var ex = new EmailNotificationException("Email notification failed");
                 ex.Data.Add("responseContent", httpContent);
@@ -58,11 +70,16 @@ public class EmailNotificationClient : IEmailNotificationClient
             }
             httpContent = await httpResponseMessage.Content.ReadAsStringAsync(ct);
 
-            var orderResponse = JsonSerializer.Deserialize<EmailOrderResponse>(httpContent);
-            return orderResponse!;
+            orderResponse = JsonSerializer.Deserialize<EmailOrderResponse>(httpContent);
+            if (orderResponse is null)
+                throw new Exception("Couldn't deserialize email notification order response");
+
+            _orderCount.WithLabels("success").Inc();
+            return orderResponse;
         }
         catch(Exception e) when (e is not EmailNotificationException)
         {
+            _orderCount.WithLabels("error").Inc();
             var ex = new EmailNotificationException("Something went wrong when processing the email order, see inner exception for details.", e);
             ex.Data.Add("responseContent", httpContent);
             ex.Data.Add("responseStatusCode", httpResponseMessage?.StatusCode.ToString());
@@ -72,6 +89,16 @@ public class EmailNotificationClient : IEmailNotificationClient
         finally
         {
             httpResponseMessage?.Dispose();
+
+            timer.Stop();
+            _telemetryClient.TrackDependency(
+                "Altinn.Notifications",
+                "OrderEmailNotification",
+                "",
+                startTime,
+                timer.Elapsed,
+                orderResponse is not null
+            );
         }
     }
 
@@ -88,16 +115,19 @@ public class EmailNotificationClient : IEmailNotificationClient
     /// <param name="platformSettings">Api endpoints for platform services.</param>
     /// <param name="appMetadata">The service providing appmetadata.</param>
     /// <param name="accessTokenGenerator">An access token generator to create an access token.</param>
+    /// <param name="telemetryClient">Client used to track dependencies.</param>
     public EmailNotificationClient(
         HttpClient httpClient,
         IOptions<PlatformSettings> platformSettings,
         IAppMetadata appMetadata,
-        IAccessTokenGenerator accessTokenGenerator)
+        IAccessTokenGenerator accessTokenGenerator,
+        TelemetryClient telemetryClient)
     {
         _httpClient = httpClient;
         _platformSettings = platformSettings.Value;
         _appMetadata = appMetadata;
         _accessTokenGenerator = accessTokenGenerator;
+        _telemetryClient = telemetryClient;
         _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
     }
 }
