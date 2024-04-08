74af0d6b647c14e0d48bf5ee4ed983a00a2a485b
HauklandJ <42615991+HauklandJ@users.noreply.github.com>
2024-04-08 10:36:41 +0200

    Gomega testing (#570)
    * test: mock server for access token test
    
    * test: rm deprecated RegisterTestingT(t)
    
    * test: api client CreateReq
    
    ---------
    
    Co-authored-by: HauklandJ <johannes.haukland@digdir.no>

--- maskinporten-poc/kubebuilder/internal/maskinporten/api_client_test.go
+++ maskinporten-poc/kubebuilder/internal/maskinporten/api_client_test.go
@@ -1,50 +1,53 @@
 package maskinporten
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	"altinn.operator/maskinporten/internal/config"
 	. "github.com/onsi/gomega"
 )
 
 func TestWellKnownConfig(t *testing.T) {
-	RegisterTestingT(t)
+	g := NewWithT(t)
 
 	cfg, err := config.LoadConfig("")
-	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(cfg).NotTo(BeNil())
 
 	apiClient, err := NewApiClient(&cfg.MaskinportenApi)
-	Expect(err).NotTo(HaveOccurred())
+	g.Expect(err).NotTo(HaveOccurred())
 
 	config1, err := apiClient.GetWellKnownConfiguration()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(config1).NotTo(BeNil())
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(config1).NotTo(BeNil())
 
 	config2, err := apiClient.GetWellKnownConfiguration()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(config2).NotTo(BeNil())
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(config2).NotTo(BeNil())
 	config3 := *config1
-	Expect(config1 == config2).To(BeTrue()) // Due to cache
-	Expect(config1 == &config3).To(BeFalse())
+	g.Expect(config1 == config2).To(BeTrue()) // Due to cache
+	g.Expect(config1 == &config3).To(BeFalse())
 }
 
 func TestCreateGrant(t *testing.T) {
-	RegisterTestingT(t)
+	g := NewWithT(t)
 
 	cfg, err := config.LoadConfig("")
-	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(cfg).NotTo(BeNil())
 
 	client, err := NewApiClient(&cfg.MaskinportenApi)
-	Expect(err).NotTo(HaveOccurred())
+	g.Expect(err).NotTo(HaveOccurred())
 
 	concreteClient, ok := client.(*apiClient)
-	Expect(ok).To(BeTrue())
+	g.Expect(ok).To(BeTrue())
 
 	grant, err := concreteClient.createGrant()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(grant).NotTo(BeNil())
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(grant).NotTo(BeNil())
 }
 
 // func TestFetchAccessToken(t *testing.T) {
@@ -64,3 +67,55 @@ func TestCreateGrant(t *testing.T) {
 // 	Expect(err).NotTo(HaveOccurred())
 // 	Expect(token.AccessToken).NotTo(BeNil())
 // }
+
+func TestFetchAccessTokenWithHTTPTest(t *testing.T) {
+	g := NewWithT(t)
+
+	// Create a mock server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"access_token":"mock_access_token","token_type":"Bearer","expires_in":3600}`))
+	}))
+	defer server.Close()
+
+	cfg, err := config.LoadConfig("")
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(cfg).NotTo(BeNil())
+
+	cfg.MaskinportenApi.Url = server.URL
+
+	client, err := NewApiClient(&cfg.MaskinportenApi)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	concreteClient, ok := client.(*apiClient)
+	g.Expect(ok).To(BeTrue())
+
+	token, err := concreteClient.accessTokenFetcher()
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(token.AccessToken).NotTo(BeNil())
+}
+
+func mockTokenRetriever() (*tokenResponse, error) {
+	// Return a mock tokenResponse
+	return &tokenResponse{AccessToken: "mock_access_token"}, nil
+}
+
+func TestCreateReq(t *testing.T) {
+	g := NewWithT(t)
+
+	client := &apiClient{
+		// Setup mock for accessToken with a custom retriever function.
+		// This Cached[tokenResponse] instance will return the mock token when Get is called.
+		accessToken: NewCached(time.Minute*5, mockTokenRetriever),
+	}
+
+	var endpoint = "http://example.com/api/endpoint"
+
+	req, err := client.CreateReq(endpoint)
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(req).NotTo(BeNil())
+	g.Expect(req.Method).To(Equal("POST"))
+	g.Expect(req.URL.String()).To(Equal(endpoint))
+	g.Expect(req.Header.Get("Content-Type")).To(Equal("application/x-www-form-urlencoded"))
+	g.Expect(req.Header.Get("Authorization")).To(Equal("Bearer mock_access_token"))
+}
