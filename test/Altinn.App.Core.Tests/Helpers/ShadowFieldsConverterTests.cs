8d87302a896753cae9460ce6349495a6a53fee23
Martin Othamar <martin.othamar@digdir.no>
2024-04-15 08:53:06 +0200

    Still include old test

--- test/Altinn.App.Core.Tests/Helpers/ShadowFieldsConverterTests.cs
+++ test/Altinn.App.Core.Tests/Helpers/ShadowFieldsConverterTests.cs
@@ -1,29 +1,28 @@
 using System.Text.Json;
-
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 using Altinn.App.Core.Helpers;
-
+using Altinn.App.Core.Tests.Implementation.TestData.AppDataModel;
 using Xunit;
 
 namespace Altinn.App.PlatformServices.Tests.Helpers;
 
 public class ShadowFieldsConverterTests
 {
-    [Fact]
-    public void ShouldRemoveShadowFields_WithPrefix()
+    private static (string Prefix, ModelWithShadowFields Data) GetData()
     {
-        const string prefix = "AltinnSF_";
-        var data = new Core.Tests.Implementation.TestData.AppDataModel.ModelWithShadowFields()
+        var data = new ModelWithShadowFields()
         {
             AltinnSF_hello = "hello",
             AltinnSF_test = "test",
             Property1 = 1,
             Property2 = 2,
-            AltinnSF_gruppeish = new Core.Tests.Implementation.TestData.AppDataModel.AltinnSF_gruppeish()
+            AltinnSF_gruppeish = new AltinnSF_gruppeish()
             {
                 F1 = "f1",
                 F2 = "f2",
             },
-            Gruppe = new List<Core.Tests.Implementation.TestData.AppDataModel.Gruppe>()
+            Gruppe = new List<Gruppe>()
             {
                 new()
                 {
@@ -37,6 +36,37 @@ public class ShadowFieldsConverterTests
                 }
             }
         };
+        return ("AltinnSF_", data);
+    }
+
+    [Fact]
+    public void ShouldRemoveShadowFields_WithPrefix()
+    {
+        var (prefix, data) = GetData();
+
+        // Check that regular serialization (without modifier) includes shadow fields in result
+        string serializedDataWithoutModifier = JsonSerializer.Serialize(data);
+        Assert.Contains(prefix, serializedDataWithoutModifier);
+
+        var modifier = new IgnorePropertiesWithPrefix("AltinnSF_");
+        JsonSerializerOptions options = new()
+        {
+            TypeInfoResolver = new DefaultJsonTypeInfoResolver
+            {
+                Modifiers = { modifier.ModifyPrefixInfo }
+            },
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        };
+
+        // Check that serialization with modifier removes shadow fields from result
+        string serializedData = JsonSerializer.Serialize(data, options);
+        Assert.DoesNotContain(prefix, serializedData);
+    }
+
+    [Fact]
+    public void ShouldRemoveShadowFields_WithPrefix_JsonHelperApi()
+    {
+        var (prefix, data) = GetData();
 
         // Check that regular serialization (without modifier) includes shadow fields in result
         string serializedDataWithoutModifier = JsonSerializer.Serialize(data);
