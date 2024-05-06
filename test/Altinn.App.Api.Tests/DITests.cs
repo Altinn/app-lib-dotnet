902e8037f32114524668cdd22f0423371f428812
Martin Othamar <martin.othamar@digdir.no>
2024-05-06 04:40:30 +0200

    Add test verifying the cause of DI issues

--- test/Altinn.App.Api.Tests/DITests.cs
+++ test/Altinn.App.Api.Tests/DITests.cs
@@ -176,4 +176,42 @@ public class DITests
         var customEvent = customEvents[0];
         Assert.Equal("TestEvent", customEvent);
     }
+
+    [Fact]
+    public async Task KeyedServices_Produces_Error_Diagnostics()
+    {
+        // This test just verifies that we rootcaused the issues re: https://github.com/Altinn/app-lib-dotnet/pull/594
+
+        using var listener = new AppInsightsListener();
+
+        var services = new ServiceCollection();
+        var env = new FakeWebHostEnvironment { EnvironmentName = "Development" };
+
+        services.AddSingleton<IWebHostEnvironment>(env);
+        services.AddSingleton<IHostingEnvironment>(env);
+
+        var config = new ConfigurationBuilder()
+            .AddInMemoryCollection(
+                [new KeyValuePair<string, string?>("ApplicationInsights:InstrumentationKey", "test")]
+            )
+            .Build();
+
+        // AppInsights SDK currently can't handle keyed services in the container
+        // Hopefully we can remove all this soon
+        services.AddKeyedSingleton<ITelemetryProcessor, TelemetryProcessor>("test");
+
+        Extensions.ServiceCollectionExtensions.AddAltinnAppServices(services, config, env);
+
+        await using (var sp = services.BuildServiceProvider())
+        {
+            var client = sp.GetService<TelemetryClient>();
+            Assert.Null(client);
+        }
+
+        await Task.Yield();
+
+        EventLevel[] errorLevels = [EventLevel.Error, EventLevel.Critical];
+        var events = listener.Events;
+        Assert.NotEmpty(events.Where(e => errorLevels.Contains(e.Level)));
+    }
 }
