c5fdee82213856d304d40fa761e5675525902d22
HauklandJ <hauklandjohannes@gmail.com>
2024-05-10 12:46:12 +0200

    chore: fix typo

--- src/Altinn.App.Core/Features/Telemetry.ProcessClient.cs
+++ src/Altinn.App.Core/Features/Telemetry.ProcessClient.cs
@@ -5,11 +5,11 @@ namespace Altinn.App.Core.Features;
 partial class Telemetry
 {
     internal Activity? StartGetProcessDefinitionActivity() =>
-        ActivitySource.StartActivity("PdfService.GetProcessDefinition");
+        ActivitySource.StartActivity("ProcessClient.GetProcessDefinition");
 
     internal Activity? StartGetProcessHistoryActivity(string? instanceId, string? instanceOwnerPartyId)
     {
-        var activity = ActivitySource.StartActivity("PdfService.GetProcessHistory");
+        var activity = ActivitySource.StartActivity("ProcessClient.GetProcessHistory");
         activity.SetInstanceId(instanceId);
         activity.SetInstanceOwnerPartyId(instanceOwnerPartyId);
         return activity;
