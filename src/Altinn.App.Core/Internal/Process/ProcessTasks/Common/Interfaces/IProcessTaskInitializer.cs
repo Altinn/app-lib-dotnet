27efb9efaaa56431f5d30fc8630973d1cf4c89bc
Daniel Skovli <daniel.skovli@gmail.com>
2024-05-10 12:16:45 +0200

    Doctag in interface declaration

--- src/Altinn.App.Core/Internal/Process/ProcessTasks/Common/Interfaces/IProcessTaskInitializer.cs
+++ src/Altinn.App.Core/Internal/Process/ProcessTasks/Common/Interfaces/IProcessTaskInitializer.cs
@@ -14,8 +14,10 @@ namespace Altinn.App.Core.Internal.Process.ProcessTasks
         /// <param name="instance"></param>
         /// <param name="prefill"></param>
         Task Initialize(string taskId, Instance instance, Dictionary<string, string>? prefill);
-        
-        // Temporary: Documented in implementation
+
+        /// <summary>
+        /// Temporary: Documented in implementation
+        /// </summary>
         Task RemoveDataElementsGeneratedFromTask(Instance instance, string taskId);
     }
 }
