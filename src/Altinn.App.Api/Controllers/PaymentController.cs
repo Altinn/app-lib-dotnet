b8245937be946dc1090b1066208532f3b19f4a1f
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2024-05-24 13:28:40 +0200

    Inherit from ControllerBase instead of Controller in PaymentController.

--- src/Altinn.App.Api/Controllers/PaymentController.cs
+++ src/Altinn.App.Api/Controllers/PaymentController.cs
@@ -18,7 +18,7 @@ namespace Altinn.App.Api.Controllers;
 [ApiController]
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [Route("{org}/{app}/instances/{instanceOwnerPartyId:int}/{instanceGuid:guid}/payment")]
-public class PaymentController : Controller
+public class PaymentController : ControllerBase
 {
     private readonly IInstanceClient _instanceClient;
     private readonly IProcessReader _processReader;
