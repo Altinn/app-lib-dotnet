136991fbca525e448d6f1341f08e0100a685223c
Ivar Nesje <ivarne@users.noreply.github.com>
2024-06-11 23:04:32 +0200

    Remove refetchBeforeExpiry
    It does not work with semaphore locking

--- src/Altinn.App.Core/Helpers/RefreshCache.cs
+++ src/Altinn.App.Core/Helpers/RefreshCache.cs
@@ -28,7 +28,7 @@ internal sealed class RefreshCache<TKey, TValue>
     /// <summary>
     /// Record to store the cached values with their expiry and refresh times
     /// </summary>
-    public record CacheEntry(DateTimeOffset Expiry, DateTimeOffset RefreshAt, TValue Value);
+    public record CacheEntry(DateTimeOffset Expiry, TValue Value);
 
     /// <summary>
     /// The collection that holds all cached items
@@ -37,7 +37,6 @@ internal sealed class RefreshCache<TKey, TValue>
 
     private const int NumLocks = 32;
     private readonly TimeProvider _timeProvider;
-    private readonly TimeSpan _refetchBeforeExpiry;
     private readonly int _maxCacheEntries;
     private readonly SemaphoreSlim[] _locks;
 
@@ -45,14 +44,12 @@ internal sealed class RefreshCache<TKey, TValue>
     /// Instantiates a new <see cref="RefreshCache{TKey,TValue}"/> instance.
     /// </summary>
     /// <param name="timeProvider">The timeprovider service.</param>
-    /// <param name="refetchBeforeExpiry">The threshold at which to refresh cache entries <em>before</em> they expire.</param>
     /// <param name="maxCacheEntries">
     /// Maximum entries to store in cache. If evictions are required, FIFO policy will be used.
     /// To allow unlimited cache entries, set this value to zero.
     /// </param>
-    public RefreshCache(TimeProvider timeProvider, TimeSpan refetchBeforeExpiry, int maxCacheEntries = 0)
+    public RefreshCache(TimeProvider timeProvider, int maxCacheEntries = 0)
     {
-        _refetchBeforeExpiry = refetchBeforeExpiry;
         _timeProvider = timeProvider;
         _maxCacheEntries = maxCacheEntries;
 
@@ -74,94 +71,42 @@ internal sealed class RefreshCache<TKey, TValue>
         Action<TValue?, CacheResultType>? postProcessCallback = default
     )
     {
+        var now = _timeProvider.GetUtcNow();
+        if (_cache.TryGetValue(key, out var entry) && entry.Expiry > now)
+        {
+            postProcessCallback?.Invoke(entry.Value, CacheResultType.Cached);
+            return entry.Value;
+        }
+
+        return await Create(key, valueFactory, lifetimeFactory, now);
+    }
+
+    private async Task<TValue> Create(
+        TKey key,
+        Func<Task<TValue>> valueFactory,
+        Func<TValue, TimeSpan> lifetimeFactory,
+        DateTimeOffset now,
+        Action<TValue?, CacheResultType>? postProcessCallback = default
+    )
+    {
         var lockNo = (key.GetHashCode() & 0x7fffffff) % NumLocks;
         var semaphore = _locks[lockNo];
         await semaphore.WaitAsync();
 
         try
         {
-            var now = _timeProvider.GetUtcNow();
-            TValue? result;
-
-            if (_cache.TryGetValue(key, out var entry))
+            // Verify that the cached was not populated while waiting on the semaphore
+            if (_cache.TryGetValue(key, out var entry) && entry.Expiry > now)
             {
-                // If the entry is still valid (with margin), return it
-                if (entry.RefreshAt > now)
-                {
-                    postProcessCallback?.Invoke(entry.Value, CacheResultType.Cached);
-                    return entry.Value;
-                }
-
-                // Kick off a new valueFactory task
-                var cacheTask = GenerateItemAndUpdateCache(
-                    key,
-                    valueFactory,
-                    lifetimeFactory,
-                    now,
-                    postProcessCallback
-                );
-
-                // Return previous value without awaiting value if it is still valid
-                if (entry.Expiry > now)
-                {
-                    postProcessCallback?.Invoke(entry.Value, CacheResultType.Refreshed);
-                    return entry.Value;
-                }
-
-                // Await the new value if the previous value has expired
-                result = await cacheTask;
-                postProcessCallback?.Invoke(result, CacheResultType.Expired);
-                return result;
+                postProcessCallback?.Invoke(entry.Value, CacheResultType.Cached);
+                return entry.Value;
             }
 
-            // No previous value found, so we need to await the task from the initializer cache
-            result = await GenerateItemAndUpdateCache(key, valueFactory, lifetimeFactory, now, postProcessCallback);
-            postProcessCallback?.Invoke(result, CacheResultType.New);
-            return result;
-        }
-        finally
-        {
-            semaphore.Release();
-        }
-    }
-
-    /// <summary>
-    /// Retrieves a value from the cache, or creates it if not present or expired.
-    /// </summary>
-    /// <param name="key">The cache key</param>
-    /// <param name="valueFactory">Factory function to create the value (if not in cache).</param>
-    /// <param name="lifetime">The lifetime of the cached value.</param>
-    /// <param name="postProcessCallback">Optional post processing callback (for metrics, etc)</param>
-    public async Task<TValue> GetOrCreate(
-        TKey key,
-        Func<Task<TValue>> valueFactory,
-        TimeSpan? lifetime = default,
-        Action<TValue?, CacheResultType>? postProcessCallback = default
-    )
-    {
-        return await GetOrCreate(
-            key: key,
-            valueFactory: valueFactory,
-            lifetimeFactory: _ => lifetime ?? DateTimeOffset.MaxValue - _timeProvider.GetUtcNow(),
-            postProcessCallback: postProcessCallback
-        );
-    }
-
-    private async Task<TValue> GenerateItemAndUpdateCache(
-        TKey key,
-        Func<Task<TValue>> valueFactory,
-        Func<TValue, TimeSpan> lifetimeFactory,
-        DateTimeOffset now,
-        Action<TValue?, CacheResultType>? processingErrorCallback = default
-    )
-    {
-        try
-        {
+            // execute the factory and create a new item
             var value = await valueFactory();
             var lifetime = lifetimeFactory(value);
             var expiry = now + lifetime;
-            var refreshAt = expiry - _refetchBeforeExpiry;
-            var cacheEntry = new CacheEntry(expiry, refreshAt, value);
+            entry = new CacheEntry(expiry, value);
 
             // Remove expired items
             foreach (var expiredItem in _cache.Where(x => x.Value.Expiry <= now))
@@ -170,34 +115,22 @@ internal sealed class RefreshCache<TKey, TValue>
             }
 
             // Store the new value
-            _cache.AddOrUpdate(key, cacheEntry, (_, _) => cacheEntry);
+            _cache.AddOrUpdate(key, entry, (_, _) => entry);
 
-            // Too many items? Prune old ones
+            // Too many items? Prune the oldest
             if (_maxCacheEntries > 0 && _cache.Count > _maxCacheEntries)
             {
-                var oldestItems = GetOverflowItems_FIFO(_maxCacheEntries);
-                foreach (var oldItem in oldestItems)
-                {
-                    _cache.TryRemove(oldItem);
-                }
+                var oldestItem = _cache.MinBy(x => x.Value.Expiry);
+                _cache.TryRemove(oldestItem);
             }
 
+            postProcessCallback?.Invoke(value, CacheResultType.New);
             return value;
         }
-        catch (Exception)
+        finally
         {
-            processingErrorCallback?.Invoke(default, CacheResultType.Error);
-            throw;
+            semaphore.Release();
         }
-    }
-
-    /// <summary>
-    /// Helper: Gets the overflowing cache entries. Eg. the items that should be removed because cache size is over its limit
-    /// </summary>
-    /// <param name="cacheSizeLimit">The cache size limit</param>
-    private IEnumerable<KeyValuePair<TKey, CacheEntry>> GetOverflowItems_FIFO(int cacheSizeLimit)
-    {
-        return _cache.OrderBy(x => x.Value.Expiry).Take(_cache.Count - cacheSizeLimit);
     }
 }
 
@@ -212,25 +145,8 @@ public enum CacheResultType
     Cached,
 
     /// <summary>
-    /// The item as cached and valid, but about to expire.
-    /// Returned as-is, but kicked off a refresh call via factory method.
-    /// </summary>
-    Refreshed,
-
-    /// <summary>
-    /// The item was in the cache, but had expired.
-    /// A new item was generated via factory method.
-    /// </summary>
-    Expired,
-
-    /// <summary>
-    /// The item was not found in the cache.
+    /// The item was not found in the cache (or it was expired).
     /// A new item was generated via factory method.
     /// </summary>
     New,
-
-    /// <summary>
-    /// An error was raised while executing the factory method for a new item.
-    /// </summary>
-    Error
 }
