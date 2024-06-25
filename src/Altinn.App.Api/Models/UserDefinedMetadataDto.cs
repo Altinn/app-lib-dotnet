fa1c451144ac9335324df8932c21b5956f03f52d
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2024-06-25 10:14:51 +0200

    file scoped namespace

--- src/Altinn.App.Api/Models/UserDefinedMetadataDto.cs
+++ src/Altinn.App.Api/Models/UserDefinedMetadataDto.cs
@@ -2,29 +2,28 @@
 using System.Text.Json.Serialization;
 using Altinn.Platform.Storage.Interface.Models;
 
-namespace Altinn.App.Api.Models
+namespace Altinn.App.Api.Models;
+
+/// <summary>
+/// Represents the response from an API endpoint providing a list of key-value properties.
+/// </summary>
+public class UserDefinedMetadataDto
 {
     /// <summary>
-    /// Represents the response from an API endpoint providing a list of key-value properties.
+    /// A list of properties represented as key-value pairs.
     /// </summary>
-    public class UserDefinedMetadataDto
-    {
-        /// <summary>
-        /// A list of properties represented as key-value pairs.
-        /// </summary>
-        [JsonPropertyName("userDefinedMetadata")]
-        public List<KeyValueEntry> UserDefinedMetadata { get; init; } = [];
+    [JsonPropertyName("userDefinedMetadata")]
+    public List<KeyValueEntry> UserDefinedMetadata { get; init; } = [];
 
-        /// <summary>
-        /// Validates that all keys in the CustomMetadata list are unique.
-        /// </summary>
-        public List<string> FindDuplicatedKeys()
-        {
-            return UserDefinedMetadata
-                .GroupBy(entry => entry.Key)
-                .Where(group => group.Count() > 1)
-                .Select(group => group.Key)
-                .ToList();
-        }
+    /// <summary>
+    /// Validates that all keys in the CustomMetadata list are unique.
+    /// </summary>
+    public List<string> FindDuplicatedKeys()
+    {
+        return UserDefinedMetadata
+            .GroupBy(entry => entry.Key)
+            .Where(group => group.Count() > 1)
+            .Select(group => group.Key)
+            .ToList();
     }
 }
