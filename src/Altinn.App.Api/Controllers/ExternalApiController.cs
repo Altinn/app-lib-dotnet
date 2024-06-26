de4d2fc51ef2a12e4ca1ec463a5c8403f00cd796
cammiida <camillamdalan@gmail.com>
2024-06-26 15:29:57 +0200

    adds first simple implementation of external api

---
+++ src/Altinn.App.Api/Controllers/ExternalApiController.cs
@@ -0,0 +1,55 @@
+using Altinn.App.Core.Features.ExternalApi;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.App.Api.Controllers
+{
+    /// <summary>
+    /// Represents the DataLists API.
+    /// </summary>
+    [Route("{org}/{app}/api/external")]
+    [ApiController]
+    public class ExternalApiController : ControllerBase
+    {
+        private readonly ILogger<ExternalApiController> _logger;
+        private readonly IExternalApiService _externalApiService;
+
+        /// <summary>
+        /// Create new instance of the <see cref="ActionsController"/> class
+        /// </summary>
+        public ExternalApiController(ILogger<ExternalApiController> logger, IExternalApiService externalApiService)
+        {
+            _logger = logger;
+            _externalApiService = externalApiService;
+        }
+
+        /// <summary>
+        /// Get the data for a specific external api
+        /// </summary>
+        /// <param name="externalApiId">The id of the external api</param>
+        /// <returns>The data for the external api</returns>
+        // [Authorize(Policy = "InstanceRead")] // TODO: Implement authorization
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("{externalApiId}")]
+        public async Task<IActionResult> Get([FromRoute] string externalApiId)
+        {
+            _logger.LogInformation("Getting data for external api with id {ExternalApiId}", externalApiId);
+
+            try
+            {
+                var externalApiData = await _externalApiService.GetExternalApiData(externalApiId);
+                return Ok(externalApiData);
+            }
+            catch (Exception e)
+            {
+                if (e is KeyNotFoundException)
+                {
+                    return NotFound();
+                }
+
+                return StatusCode(500, "An error occurred while fetching data from an external api.");
+            }
+        }
+    }
+}
---
+++ src/Altinn.App.Core/Features/ExternalApi/ExternalApiFactory.cs
@@ -0,0 +1,48 @@
+using Microsoft.Extensions.Logging;
+
+namespace Altinn.App.Core.Features.ExternalApi
+{
+    /// <summary>
+    /// Factory class for resolving <see cref="IExternalApiClient"/> implementations
+    /// </summary>
+    public class ExternalApiFactory
+    {
+        private readonly ILogger<ExternalApiFactory> _logger;
+        private IEnumerable<IExternalApiClient> ExternalApiClients { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExternalApiFactory"/> class.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="externalApiClients"></param>
+        public ExternalApiFactory(
+            ILogger<ExternalApiFactory> logger,
+            IEnumerable<IExternalApiClient> externalApiClients
+        )
+        {
+            _logger = logger;
+            ExternalApiClients = externalApiClients;
+        }
+
+        /// <summary>
+        ///  Finds the implementation of IExternalApiClient based on the external api id
+        /// </summary>
+        /// <param name="externalApiId"></param>
+        public IExternalApiClient GetExternalApiClient(string externalApiId)
+        {
+            IExternalApiClient? client = ExternalApiClients.FirstOrDefault(e =>
+                e.Id.Equals(externalApiId, StringComparison.OrdinalIgnoreCase)
+            );
+
+            if (client == null)
+            {
+                _logger.LogWarning("No external client found for external API with id {ExternalApiId}", externalApiId);
+                throw new KeyNotFoundException(
+                    $"No external api client found for external API with id {externalApiId}"
+                );
+            }
+
+            return client;
+        }
+    }
+}
