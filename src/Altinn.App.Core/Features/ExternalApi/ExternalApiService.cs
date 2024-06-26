c9e6ba8268fa9faaec5ee1b3a4f3183cec950f9c
cammiida <camillamdalan@gmail.com>
2024-06-26 15:29:57 +0200

    adds first simple implementation of external api

---
+++ src/Altinn.App.Core/Features/ExternalApi/ExternalApiService.cs
@@ -0,0 +1,46 @@
+using Microsoft.Extensions.Logging;
+
+namespace Altinn.App.Core.Features.ExternalApi
+{
+    /// <summary>
+    /// Interface for handling external api data
+    /// </summary>
+    public interface IExternalApiService
+    {
+        /// <summary>
+        /// Get data for an external api
+        /// </summary>
+        /// <param name="externalApiId"></param>
+        /// <returns>An arbitrary json data object</returns>
+        Task<object?> GetExternalApiData(string externalApiId);
+    }
+
+    /// <summary>
+    /// Service for handling external api data
+    /// </summary>
+    public class ExternalApiService : IExternalApiService
+    {
+        private readonly ILogger<ExternalApiService> _logger;
+        private readonly ExternalApiFactory _externalApiFactory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExternalApiService"/> class.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="externalApiFactory"></param>
+        public ExternalApiService(ILogger<ExternalApiService> logger, ExternalApiFactory externalApiFactory)
+        {
+            _logger = logger;
+            _externalApiFactory = externalApiFactory;
+        }
+
+        /// <inheritdoc/>
+        public async Task<object?> GetExternalApiData(string externalApiId)
+        {
+            _logger.LogInformation("Getting data for external api with id {ExternalApiId}", externalApiId);
+            var externalApiClient = _externalApiFactory.GetExternalApiClient(externalApiId);
+
+            return await externalApiClient.GetExternalApiDataAsync(externalApiId);
+        }
+    }
+}
---
+++ src/Altinn.App.Core/Features/ExternalApi/IExternalApiClient.cs
@@ -0,0 +1,20 @@
+namespace Altinn.App.Core.Features.ExternalApi
+{
+    /// <summary>
+    /// Interface for providing external api data
+    /// </summary>
+    public interface IExternalApiClient
+    {
+        /// <summary>
+        /// The id/name that is used in the <c>externalApiId</c> parameter in the ExternalApiController
+        /// </summary>
+        string Id { get; }
+
+        /// <summary>
+        /// Fetches data from the external api
+        /// </summary>
+        /// <param name="externalApiId"></param>
+        /// <returns></returns>
+        Task<object?> GetExternalApiDataAsync(string externalApiId);
+    }
+}
