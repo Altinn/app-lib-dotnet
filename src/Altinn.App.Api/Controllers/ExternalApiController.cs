ac589ba72fbbf5190da359bdfe739e83859a62fd
cammiida <camillamdalan@gmail.com>
2024-06-27 10:07:02 +0200

    uses file scoped namespaces for external api files

--- src/Altinn.App.Api/Controllers/ExternalApiController.cs
+++ src/Altinn.App.Api/Controllers/ExternalApiController.cs
@@ -1,55 +1,54 @@
 using Altinn.App.Core.Features.ExternalApi;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Altinn.App.Api.Controllers
+namespace Altinn.App.Api.Controllers;
+
+/// <summary>
+/// Represents the DataLists API.
+/// </summary>
+[Route("{org}/{app}/api/external")]
+[ApiController]
+public class ExternalApiController : ControllerBase
 {
+    private readonly ILogger<ExternalApiController> _logger;
+    private readonly IExternalApiService _externalApiService;
+
     /// <summary>
-    /// Represents the DataLists API.
+    /// Create new instance of the <see cref="ActionsController"/> class
     /// </summary>
-    [Route("{org}/{app}/api/external")]
-    [ApiController]
-    public class ExternalApiController : ControllerBase
+    public ExternalApiController(ILogger<ExternalApiController> logger, IExternalApiService externalApiService)
     {
-        private readonly ILogger<ExternalApiController> _logger;
-        private readonly IExternalApiService _externalApiService;
+        _logger = logger;
+        _externalApiService = externalApiService;
+    }
 
-        /// <summary>
-        /// Create new instance of the <see cref="ActionsController"/> class
-        /// </summary>
-        public ExternalApiController(ILogger<ExternalApiController> logger, IExternalApiService externalApiService)
+    /// <summary>
+    /// Get the data for a specific external api
+    /// </summary>
+    /// <param name="externalApiId">The id of the external api</param>
+    /// <returns>The data for the external api</returns>
+    // [Authorize(Policy = "InstanceRead")] // TODO: Implement authorization
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Route("{externalApiId}")]
+    public async Task<IActionResult> Get([FromRoute] string externalApiId)
+    {
+        _logger.LogInformation("Getting data for external api with id {ExternalApiId}", externalApiId);
+
+        try
         {
-            _logger = logger;
-            _externalApiService = externalApiService;
+            var externalApiData = await _externalApiService.GetExternalApiData(externalApiId);
+            return Ok(externalApiData);
         }
-
-        /// <summary>
-        /// Get the data for a specific external api
-        /// </summary>
-        /// <param name="externalApiId">The id of the external api</param>
-        /// <returns>The data for the external api</returns>
-        // [Authorize(Policy = "InstanceRead")] // TODO: Implement authorization
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [Route("{externalApiId}")]
-        public async Task<IActionResult> Get([FromRoute] string externalApiId)
+        catch (Exception e)
         {
-            _logger.LogInformation("Getting data for external api with id {ExternalApiId}", externalApiId);
-
-            try
+            if (e is KeyNotFoundException)
             {
-                var externalApiData = await _externalApiService.GetExternalApiData(externalApiId);
-                return Ok(externalApiData);
+                return NotFound();
             }
-            catch (Exception e)
-            {
-                if (e is KeyNotFoundException)
-                {
-                    return NotFound();
-                }
 
-                return StatusCode(500, "An error occurred while fetching data from an external api.");
-            }
+            return StatusCode(500, "An error occurred while fetching data from an external api.");
         }
     }
 }
--- src/Altinn.App.Core/Features/ExternalApi/ExternalApiFactory.cs
+++ src/Altinn.App.Core/Features/ExternalApi/ExternalApiFactory.cs
@@ -1,48 +1,47 @@
 using Microsoft.Extensions.Logging;
 
-namespace Altinn.App.Core.Features.ExternalApi
+namespace Altinn.App.Core.Features.ExternalApi;
+
+/// <summary>
+/// Factory class for resolving <see cref="IExternalApiClient"/> implementations
+/// </summary>
+public class ExternalApiFactory
 {
+    private readonly ILogger<ExternalApiFactory> _logger;
+    private IEnumerable<IExternalApiClient> ExternalApiClients { get; }
+
     /// <summary>
-    /// Factory class for resolving <see cref="IExternalApiClient"/> implementations
+    /// Initializes a new instance of the <see cref="ExternalApiFactory"/> class.
     /// </summary>
-    public class ExternalApiFactory
+    /// <param name="logger"></param>
+    /// <param name="externalApiClients"></param>
+    public ExternalApiFactory(
+        ILogger<ExternalApiFactory> logger,
+        IEnumerable<IExternalApiClient> externalApiClients
+    )
     {
-        private readonly ILogger<ExternalApiFactory> _logger;
-        private IEnumerable<IExternalApiClient> ExternalApiClients { get; }
+        _logger = logger;
+        ExternalApiClients = externalApiClients;
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ExternalApiFactory"/> class.
-        /// </summary>
-        /// <param name="logger"></param>
-        /// <param name="externalApiClients"></param>
-        public ExternalApiFactory(
-            ILogger<ExternalApiFactory> logger,
-            IEnumerable<IExternalApiClient> externalApiClients
-        )
-        {
-            _logger = logger;
-            ExternalApiClients = externalApiClients;
-        }
+    /// <summary>
+    ///  Finds the implementation of IExternalApiClient based on the external api id
+    /// </summary>
+    /// <param name="externalApiId"></param>
+    public IExternalApiClient GetExternalApiClient(string externalApiId)
+    {
+        IExternalApiClient? client = ExternalApiClients.FirstOrDefault(e =>
+            e.Id.Equals(externalApiId, StringComparison.OrdinalIgnoreCase)
+        );
 
-        /// <summary>
-        ///  Finds the implementation of IExternalApiClient based on the external api id
-        /// </summary>
-        /// <param name="externalApiId"></param>
-        public IExternalApiClient GetExternalApiClient(string externalApiId)
+        if (client == null)
         {
-            IExternalApiClient? client = ExternalApiClients.FirstOrDefault(e =>
-                e.Id.Equals(externalApiId, StringComparison.OrdinalIgnoreCase)
+            _logger.LogWarning("No external client found for external API with id {ExternalApiId}", externalApiId);
+            throw new KeyNotFoundException(
+                $"No external api client found for external API with id {externalApiId}"
             );
-
-            if (client == null)
-            {
-                _logger.LogWarning("No external client found for external API with id {ExternalApiId}", externalApiId);
-                throw new KeyNotFoundException(
-                    $"No external api client found for external API with id {externalApiId}"
-                );
-            }
-
-            return client;
         }
+
+        return client;
     }
 }
