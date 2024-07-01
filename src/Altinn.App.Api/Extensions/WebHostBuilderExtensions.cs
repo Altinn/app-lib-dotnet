b9fdae684bfbcc0ad33a226f638e00c0fcb60050
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2024-07-01 12:10:36 +0200

    Add extension to IHostBuilder instead of IWebHostBuilder.

--- src/Altinn.App.Api/Extensions/WebHostBuilderExtensions.cs
+++ src/Altinn.App.Api/Extensions/WebHostBuilderExtensions.cs
@@ -35,45 +35,4 @@ public static class WebHostBuilderExtensions
             }
         );
     }
-
-    /// <summary>
-    /// Add KeyVault as a configuration provider. Requires that the kvSetting section is present in the configuration and throws an exception if not. See documentation for secret handling in Altinn apps.
-    /// </summary>
-    /// <param name="builder"></param>
-    public static void AddKeyVaultAsConfigProvider(this IWebHostBuilder builder)
-    {
-        builder.ConfigureAppConfiguration(
-            (_, configBuilder) =>
-            {
-                IConfiguration stageOneConfig = configBuilder.Build();
-                var keyVaultSettings = new KeyVaultSettings();
-                stageOneConfig.GetSection("kvSetting").Bind(keyVaultSettings);
-
-                if (
-                    string.IsNullOrEmpty(keyVaultSettings.ClientId)
-                    || string.IsNullOrEmpty(keyVaultSettings.TenantId)
-                    || string.IsNullOrEmpty(keyVaultSettings.ClientSecret)
-                    || string.IsNullOrEmpty(keyVaultSettings.SecretUri)
-                )
-                {
-                    throw new ApplicationConfigException(
-                        "Attempted to add KeyVault as a configuration provider, but the required settings for authenticating with KeyVault are missing. Please check the configuration."
-                    );
-                }
-
-                var clientSecretCredential = new ClientSecretCredential(
-                    keyVaultSettings.TenantId,
-                    keyVaultSettings.ClientId,
-                    keyVaultSettings.ClientSecret
-                );
-
-                var secretClient = new SecretClient(new Uri(keyVaultSettings.SecretUri), clientSecretCredential);
-
-                configBuilder.AddAzureKeyVault(
-                    secretClient,
-                    new Azure.Extensions.AspNetCore.Configuration.Secrets.KeyVaultSecretManager()
-                );
-            }
-        );
-    }
 }
