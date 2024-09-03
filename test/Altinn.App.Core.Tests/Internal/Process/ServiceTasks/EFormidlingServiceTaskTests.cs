91a07b0fdd38ca1de7a50be03b5ae55e712ec195
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2024-09-03 09:04:55 +0200

    Unit test service tasks.

---
+++ test/Altinn.App.Core.Tests/Internal/Process/ServiceTasks/EFormidlingServiceTaskTests.cs
@@ -0,0 +1,98 @@
+﻿using Altinn.App.Core.Configuration;
+using Altinn.App.Core.EFormidling.Interface;
+using Altinn.App.Core.Internal.Instances;
+using Altinn.App.Core.Internal.Process;
+using Altinn.App.Core.Internal.Process.ProcessTasks.ServiceTasks;
+using Altinn.Platform.Storage.Interface.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace Altinn.App.Core.Tests.Internal.Process.ServiceTasks;
+
+public class EFormidlingServiceTaskTests
+{
+    private readonly Mock<ILogger<EFormidlingServiceTask>> _loggerMock;
+    private readonly Mock<IInstanceClient> _instanceClientMock;
+    private readonly Mock<IEFormidlingService> _eFormidlingServiceMock;
+    private readonly Mock<IOptions<AppSettings>> _appSettingsMock;
+
+    private readonly EFormidlingServiceTask _serviceTask;
+
+    public EFormidlingServiceTaskTests()
+    {
+        _loggerMock = new Mock<ILogger<EFormidlingServiceTask>>();
+        _instanceClientMock = new Mock<IInstanceClient>();
+        _eFormidlingServiceMock = new Mock<IEFormidlingService>();
+        _appSettingsMock = new Mock<IOptions<AppSettings>>();
+
+        _serviceTask = new EFormidlingServiceTask(
+            _loggerMock.Object,
+            _instanceClientMock.Object,
+            _eFormidlingServiceMock.Object,
+            _appSettingsMock.Object
+        );
+    }
+
+    [Fact]
+    public async Task Execute_Should_LogWarning_When_EFormidlingDisabled()
+    {
+        // Arrange
+        var instance = new Instance();
+        var appSettings = new AppSettings { EnableEFormidling = false };
+        _appSettingsMock.Setup(x => x.Value).Returns(appSettings);
+
+        // Act
+        await _serviceTask.Execute("taskId", instance);
+
+        // Assert
+        _loggerMock.Verify(
+            x =>
+                x.Log(
+                    LogLevel.Warning,
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>(
+                        (v, t) => v.ToString()!.Contains("EFormidling is not enabled in appsettings.json")
+                    ),
+                    It.IsAny<Exception>(),
+                    It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)
+                ),
+            Times.Once
+        );
+    }
+
+    [Fact]
+    public async Task Execute_Should_ThrowException_When_EFormidlingServiceIsNull()
+    {
+        // Arrange
+        var instance = new Instance();
+
+        var appSettings = new AppSettings { EnableEFormidling = true };
+        _appSettingsMock.Setup(x => x.Value).Returns(appSettings);
+
+        var serviceTask = new EFormidlingServiceTask(
+            _loggerMock.Object,
+            _instanceClientMock.Object,
+            null,
+            _appSettingsMock.Object
+        );
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ProcessException>(() => serviceTask.Execute("taskId", instance));
+    }
+
+    [Fact]
+    public async Task Execute_Should_Call_SendEFormidlingShipment_When_EFormidlingEnabled()
+    {
+        // Arrange
+        var instance = new Instance();
+        var appSettings = new AppSettings { EnableEFormidling = true };
+        _appSettingsMock.Setup(x => x.Value).Returns(appSettings);
+
+        // Act
+        await _serviceTask.Execute("taskId", instance);
+
+        // Assert
+        _eFormidlingServiceMock.Verify(x => x.SendEFormidlingShipment(instance), Times.Once);
+    }
+}
---
+++ test/Altinn.App.Core.Tests/Internal/Process/ServiceTasks/PdfServiceTaskTests.cs
@@ -0,0 +1,37 @@
+﻿using Altinn.App.Core.Internal.Pdf;
+using Altinn.App.Core.Internal.Process.ProcessTasks.ServiceTasks;
+using Altinn.Platform.Storage.Interface.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Altinn.App.Core.Tests.Internal.Process.ServiceTasks;
+
+public class PdfServiceTaskTests
+{
+    private readonly Mock<ILogger<PdfServiceTask>> _loggerMock;
+    private readonly Mock<IPdfService> _pdfServiceMock;
+
+    private readonly PdfServiceTask _serviceTask;
+
+    public PdfServiceTaskTests()
+    {
+        _loggerMock = new Mock<ILogger<PdfServiceTask>>();
+        _pdfServiceMock = new Mock<IPdfService>();
+
+        _serviceTask = new PdfServiceTask(_loggerMock.Object, _pdfServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task Execute_Should_Call_GenerateAndStorePdf()
+    {
+        // Arrange
+        var instance = new Instance();
+        var taskId = "taskId";
+
+        // Act
+        await _serviceTask.Execute(taskId, instance);
+
+        // Assert
+        _pdfServiceMock.Verify(x => x.GenerateAndStorePdf(instance, taskId, It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
