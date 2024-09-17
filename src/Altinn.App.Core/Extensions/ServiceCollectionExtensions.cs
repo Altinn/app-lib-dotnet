84d00e050ecd4994e9ee7d2c1d72771917cf1286
Martin Othamar <martin.othamar@digdir.no>
2024-09-17 12:25:14 +0200

    AppImplementatinFactory

--- src/Altinn.App.Core/Extensions/ServiceCollectionExtensions.cs
+++ src/Altinn.App.Core/Extensions/ServiceCollectionExtensions.cs
@@ -152,6 +152,8 @@ public static class ServiceCollectionExtensions
         IWebHostEnvironment env
     )
     {
+        services.AddAppImplementationFactory();
+
         // Services for Altinn App
         services.TryAddTransient<IPDP, PDPAppSI>();
         AddValidationServices(services, configuration);
