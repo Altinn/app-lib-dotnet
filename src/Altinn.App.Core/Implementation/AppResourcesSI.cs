ef36453c1796a8ea3c3616006ba6490964a3afa6
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2024-10-09 09:32:24 +0200

    Return empty LayoutModel from GetLayoutModel if there is no layout-set for the current task. Done in order to be able to evaluate expressions in gateway while current task is a service task with no layout-set.

--- src/Altinn.App.Core/Implementation/AppResourcesSI.cs
+++ src/Altinn.App.Core/Implementation/AppResourcesSI.cs
@@ -314,16 +314,20 @@ public class AppResourcesSI : IAppResources
     public LayoutModel GetLayoutModel(string? layoutSetId = null)
     {
         using var activity = _telemetry?.StartGetLayoutModelActivity();
-        string folder = Path.Join(_settings.AppBasePath, _settings.UiFolder, layoutSetId, "layouts");
-        var order = GetLayoutSettingsForSet(layoutSetId)?.Pages?.Order;
-        if (order is null)
-        {
-            throw new InvalidDataException(
-                "No $Pages.Order field found" + (layoutSetId is null ? "" : $" for layoutSet {layoutSetId}")
-            );
-        }
 
         var layoutModel = new LayoutModel();
+        if (layoutSetId is null)
+        {
+            return layoutModel;
+        }
+
+        string folder = Path.Join(_settings.AppBasePath, _settings.UiFolder, layoutSetId, "layouts");
+        List<string>? order = GetLayoutSettingsForSet(layoutSetId)?.Pages?.Order;
+        if (order is null)
+        {
+            throw new InvalidDataException("No $Pages.Order field found for layoutSet {layoutSetId}");
+        }
+
         foreach (var page in order)
         {
             var pageBytes = File.ReadAllBytes(Path.Join(folder, page + ".json"));
