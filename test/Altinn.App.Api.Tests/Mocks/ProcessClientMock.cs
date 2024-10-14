398b6ce6ff3c5b7fb94ebc6ec842c9452c4c9bab
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2024-10-14 10:20:41 +0200

    Mock telemetry in ProcessClientMock.

--- test/Altinn.App.Api.Tests/Mocks/ProcessClientMock.cs
+++ test/Altinn.App.Api.Tests/Mocks/ProcessClientMock.cs
@@ -1,21 +1,33 @@
 ﻿using Altinn.App.Core.Configuration;
+using Altinn.App.Core.Features;
+using Altinn.App.Core.Infrastructure.Clients.Storage;
 using Altinn.App.Core.Internal.Process;
 using Altinn.Platform.Storage.Interface.Models;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Altinn.App.Api.Tests.Mocks;
 
 public class ProcessClientMock : IProcessClient
 {
+    private readonly ILogger<ProcessClient> _logger;
+    private readonly Telemetry? _telemetry;
     private readonly AppSettings _appSettings;
 
-    public ProcessClientMock(IOptions<AppSettings> appSettings)
+    public ProcessClientMock(
+        IOptions<AppSettings> appSettings,
+        ILogger<ProcessClient> logger,
+        Telemetry? telemetry = null
+    )
     {
         _appSettings = appSettings.Value;
+        _logger = logger;
+        _telemetry = telemetry;
     }
 
     public Stream GetProcessDefinition()
     {
+        using var activity = _telemetry?.StartGetProcessDefinitionActivity();
         string bpmnFilePath = Path.Join(
             _appSettings.AppBasePath,
             _appSettings.ConfigurationFolder,
@@ -23,8 +35,19 @@ public class ProcessClientMock : IProcessClient
             _appSettings.ProcessFileName
         );
 
-        Stream processModel = File.OpenRead(bpmnFilePath);
-        return processModel;
+        try
+        {
+            Stream processModel = File.OpenRead(bpmnFilePath);
+
+            return processModel;
+        }
+        catch (Exception processDefinitionException)
+        {
+            _logger.LogError(
+                $"Cannot find process definition file for this app. Have tried file location {bpmnFilePath}. Exception {processDefinitionException}"
+            );
+            throw;
+        }
     }
 
     public Task<ProcessHistoryList> GetProcessHistory(string instanceGuid, string instanceOwnerPartyId)
