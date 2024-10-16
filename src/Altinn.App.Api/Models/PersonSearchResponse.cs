565edf6744dc3a28c6be991729685a9305d71118
Bjørn Tore Gjerde <bjorn-tore.gjerde@digdir.no>
2024-10-16 08:22:50 +0200

    Expose endpoint for searching for person using ssn and last name. (#815)
    * Expose endpoint for searching for person using ssn and last name.
    
    * Use class and drop dto postfix.
    
    * Move mapping to reponse class.
    
    * Add success property to PersonSearchResult. Move person details into nested object.

---
+++ src/Altinn.App.Api/Models/PersonSearchResponse.cs
@@ -0,0 +1,77 @@
+﻿using Altinn.Platform.Register.Models;
+
+namespace Altinn.App.Api.Models;
+
+/// <summary>
+/// Contains the result of a person search request.
+/// </summary>
+public class PersonSearchResponse
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="PersonSearchResponse"/> from a person and sets the <see cref="Success"/> and <see cref="PersonDetails"/> properties accordingly.
+    /// </summary>
+    public static PersonSearchResponse CreateFromPerson(Person? person)
+    {
+        return new PersonSearchResponse
+        {
+            Success = person is not null,
+            PersonDetails = person is not null ? PersonDetails.MapFromPerson(person) : null,
+        };
+    }
+
+    /// <summary>
+    /// Indicates whether a person was found or not.
+    /// </summary>
+    public bool Success { get; init; }
+
+    /// <summary>
+    /// Contains details about the person found by the search. Null if no person was found.
+    /// </summary>
+    public PersonDetails? PersonDetails { get; init; }
+}
+
+/// <summary>
+/// Contains details about a person
+/// </summary>
+public class PersonDetails
+{
+    /// <summary>
+    /// The social security number
+    /// </summary>
+    public required string Ssn { get; init; }
+
+    /// <summary>
+    /// The full name
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// The first name
+    /// </summary>
+    public string? FirstName { get; init; }
+
+    /// <summary>
+    /// The middle name
+    /// </summary>
+    public string? MiddleName { get; init; }
+
+    /// <summary>
+    /// The last name
+    /// </summary>
+    public required string LastName { get; init; }
+
+    /// <summary>
+    /// Maps a person to person details
+    /// </summary>
+    public static PersonDetails MapFromPerson(Person person)
+    {
+        return new PersonDetails
+        {
+            Ssn = person.SSN,
+            Name = person.Name,
+            FirstName = person.FirstName,
+            MiddleName = person.MiddleName,
+            LastName = person.LastName,
+        };
+    }
+}
