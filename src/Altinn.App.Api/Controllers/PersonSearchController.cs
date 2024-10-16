24351da8fc2dfbfdade3e6cd815150cc44502f4b
Bjørn Tore Gjerde <bjorn-tore.gjerde@digdir.no>
2024-10-16 08:22:50 +0200

    Expose endpoint for searching for person using ssn and last name. (#815)
    * Expose endpoint for searching for person using ssn and last name.
    
    * Use class and drop dto postfix.
    
    * Move mapping to reponse class.
    
    * Add success property to PersonSearchResult. Move person details into nested object.

---
+++ src/Altinn.App.Api/Controllers/PersonSearchController.cs
@@ -0,0 +1,53 @@
+using System.Net.Mime;
+using Altinn.App.Api.Infrastructure.Filters;
+using Altinn.App.Api.Models;
+using Altinn.App.Core.Internal.Registers;
+using Altinn.Platform.Register.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.App.Api.Controllers;
+
+/// <summary>
+/// This controller class provides Folkeregisteret (DSF) person search functionality.
+/// </summary>
+[AutoValidateAntiforgeryTokenIfAuthCookie]
+[ApiController]
+[Produces(MediaTypeNames.Application.Json)]
+[Consumes(MediaTypeNames.Application.Json)]
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[Route("{org}/{app}/api/v1/person-search")]
+public class PersonSearchControllerController : ControllerBase
+{
+    private readonly IPersonClient _personClient;
+
+    /// <summary>
+    /// Initialize a new instance of <see cref="PersonSearchControllerController"/> with the given services.
+    /// </summary>
+    /// <param name="personClient">A client for searching for a person.</param>
+    public PersonSearchControllerController(IPersonClient personClient)
+    {
+        _personClient = personClient;
+    }
+
+    /// <summary>
+    /// Allows searching for a person in Folkeregisteret (DSF)
+    /// </summary>
+    /// <param name="personSearchRequest">Payload that contains params for executing a person search.</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>A <see cref="PersonSearchResponse"/> object.</returns>
+    [HttpPost]
+    [ProducesResponseType(typeof(PersonSearchResponse), StatusCodes.Status200OK)]
+    public async Task<ActionResult<PersonSearchResponse>> SearchForPerson(
+        [FromBody] PersonSearchRequest personSearchRequest,
+        CancellationToken cancellationToken
+    )
+    {
+        Person? person = await _personClient.GetPerson(
+            personSearchRequest.SocialSecurityNumber,
+            personSearchRequest.LastName,
+            cancellationToken
+        );
+
+        return Ok(PersonSearchResponse.CreateFromPerson(person));
+    }
+}
