0f82cc55806ce86d3b171b43fb822f6143f95ad0
Adam Haeger <adamgullerud@gmail.com>
2024-11-27 10:11:20 +0100

    added settings to initial state

--- src/Altinn.App.Api/Controllers/InitialStateController.cs
+++ src/Altinn.App.Api/Controllers/InitialStateController.cs
@@ -1,7 +1,10 @@
+using System.Text.Json;
+using Altinn.App.Core.Configuration;
 using Altinn.App.Core.Internal.App;
 using Altinn.App.Core.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace Altinn.App.Api.Controllers;
 
@@ -15,17 +18,31 @@ public class InitialStateController : ControllerBase
 {
     private readonly IAppMetadata _appMetadata;
     private readonly ILogger<ApplicationMetadataController> _logger;
+    private readonly AppSettings _appSettings;
+    private readonly FrontEndSettings _frontEndSettings;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="ApplicationMetadataController"/> class
     /// <param name="appMetadata">The IAppMetadata service</param>
     /// <param name="logger">Logger for ApplicationMetadataController</param>
     /// </summary>
-    public InitialStateController(IAppMetadata appMetadata, ILogger<ApplicationMetadataController> logger)
+    public InitialStateController(
+        IAppMetadata appMetadata,
+        ILogger<ApplicationMetadataController> logger,
+        IOptions<AppSettings> appSettings,
+        IOptions<FrontEndSettings> frontEndSettings
+    )
     {
         _appMetadata = appMetadata;
         _logger = logger;
+        _appSettings = appSettings.Value;
+        _frontEndSettings = frontEndSettings.Value;
     }
+
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+    };
 
     /// <summary>
     /// Get the application metadata https://altinncdn.no/schemas/json/application/application-metadata.schema.v1.json
@@ -37,81 +54,39 @@ public class InitialStateController : ControllerBase
     /// <param name="checkOrgApp">Boolean get parameter to skip verification of correct org/app</param>
     /// <returns>Application metadata</returns>
     [HttpGet("{org}/{app}/api/v1/inital-state")]
-    public async Task<ActionResult<ApplicationMetadata>> GetAction(
-        string org,
-        string app,
-        [FromQuery] bool checkOrgApp = true
-    )
+    public async Task<ActionResult<InitialState>> GetAction(string org, string app, [FromQuery] bool checkOrgApp = true)
     {
         ApplicationMetadata application = await _appMetadata.GetApplicationMetadata();
 
         string wantedAppId = $"{org}/{app}";
+
+        var initialState = new InitialState(application);
+        // {
+        //     ApplicationMetadata = application,
+        //     // Populate any additional properties of InitialState if necessary
+        // };
+
+
+
+        FrontEndSettings frontEndSettings = new FrontEndSettings();
+
+        // Adding key from _appSettings to be backwards compatible.
+        if (
+            !frontEndSettings.ContainsKey(nameof(_appSettings.AppOidcProvider))
+            && !string.IsNullOrEmpty(_appSettings.AppOidcProvider)
+        )
+        {
+            frontEndSettings.Add(nameof(_appSettings.AppOidcProvider), _appSettings.AppOidcProvider);
+        }
+
+        //return new JsonResult(frontEndSettings, _jsonSerializerOptions);
+        initialState.FrontEndSettings = new JsonResult(frontEndSettings, _jsonSerializerOptions);
 
         if (!checkOrgApp || application.Id.Equals(wantedAppId, StringComparison.Ordinal))
         {
-            return Ok(application);
+            return Ok(initialState);
         }
 
         return Conflict($"This is {application.Id}, and not the app you are looking for: {wantedAppId}!");
-    }
-
-    /// <summary>
-    /// Get the application XACML policy file
-    ///
-    /// If org and app does not match, this returns a 409 Conflict response
-    /// </summary>
-    /// <param name="org">Unique identifier of the organisation responsible for the app.</param>
-    /// <param name="app">Application identifier which is unique within an organisation.</param>
-    /// <returns>XACML policy file</returns>
-    [HttpGet("{org}/{app}/api/v1/meta/authorizationpolicy")]
-    public async Task<ActionResult<string>> GetPolicy(string org, string app)
-    {
-        ApplicationMetadata application = await _appMetadata.GetApplicationMetadata();
-        try
-        {
-            string policy = await _appMetadata.GetApplicationXACMLPolicy();
-            string wantedAppId = $"{org}/{app}";
-
-            if (application.Id.Equals(wantedAppId, StringComparison.Ordinal))
-            {
-                return Content(policy, "text/xml", System.Text.Encoding.UTF8);
-            }
-
-            return Conflict($"This is {application.Id}, and not the app you are looking for: {wantedAppId}!");
-        }
-        catch (FileNotFoundException)
-        {
-            return NotFound();
-        }
-    }
-
-    /// <summary>
-    /// Get the application BPMN process file
-    ///
-    /// If org and app does not match, this returns a 409 Conflict response
-    /// </summary>
-    /// <param name="org">Unique identifier of the organisation responsible for the app.</param>
-    /// <param name="app">Application identifier which is unique within an organisation.</param>
-    /// <returns>BPMN process file</returns>
-    [HttpGet("{org}/{app}/api/v1/meta/process")]
-    public async Task<ActionResult<string>> GetProcess(string org, string app)
-    {
-        ApplicationMetadata application = await _appMetadata.GetApplicationMetadata();
-        string wantedAppId = $"{org}/{app}";
-        try
-        {
-            if (application.Id.Equals(wantedAppId, StringComparison.Ordinal))
-            {
-                string process = await _appMetadata.GetApplicationBPMNProcess();
-                return Content(process, "text/xml", System.Text.Encoding.UTF8);
-            }
-
-            return Conflict($"This is {application.Id}, and not the app you are looking for: {wantedAppId}!");
-        }
-        catch (ApplicationConfigException ex)
-        {
-            _logger.LogError(ex, "Failed to read process from file for appId: ${WantedApp}", wantedAppId);
-            return NotFound();
-        }
     }
 }
---
+++ src/Altinn.App.Core/Models/InitialState.cs
@@ -0,0 +1,18 @@
+using Altinn.App.Core.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.App.Core.Models;
+
+using System.Text.Json;
+
+public class InitialState
+{
+    public InitialState(ApplicationMetadata metadata)
+    {
+        ApplicationMetadata = metadata;
+    }
+
+    public ApplicationMetadata ApplicationMetadata { get; set; }
+
+    public JsonResult FrontEndSettings { get; set; }
+}
