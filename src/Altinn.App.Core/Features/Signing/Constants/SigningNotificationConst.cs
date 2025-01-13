a5eea3ede76ff5470c09841cfd287694176545ed
HauklandJ <hauklandjohannes@gmail.com>
2025-01-13 09:00:23 +0100

    update default sms body

--- src/Altinn.App.Core/Features/Signing/Constants/SigningNotificationConst.cs
+++ src/Altinn.App.Core/Features/Signing/Constants/SigningNotificationConst.cs
@@ -3,7 +3,7 @@ namespace Altinn.App.Core.Features.Signing.Constants;
 internal sealed class SigningNotificationConst
 {
     internal const string DefaultSmsBody =
-        "Du har mottatt en ny oppgave i ALTINN. Åpne din ALTINN-innboks for å fullføre oppgaven.";
+        "Du har mottatt en oppgave til signering. Du finner oppgaven i innboksen i Altinn.";
     internal const string DefaultEmailBody =
-        "Du har mottatt en ny oppgave i ALTINN. Åpne din ALTINN-innboks for å fullføre oppgaven.";
+        "Du har mottatt en oppgave til signering. Du finner oppgaven i innboksen i Altinn.";
 }
