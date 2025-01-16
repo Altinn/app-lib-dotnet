f3273bac4f2fedbab8046b9bff11baea68c56ee4
HauklandJ <hauklandjohannes@gmail.com>
2025-01-16 10:15:48 +0100

    add signing delegation tests

---
+++ test/Altinn.App.Core.Tests/Features/Signing/Delegation/SigningDelegationServiceTests.cs
@@ -0,0 +1,175 @@
+using Altinn.App.Core.Features.Signing;
+using Altinn.App.Core.Features.Signing.Models;
+using Altinn.App.Core.Internal.AccessManagement;
+using Altinn.App.Core.Internal.AccessManagement.Models;
+using Altinn.App.Core.Models;
+using Altinn.Platform.Register.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Altinn.App.Core.Tests.Features.Signing.Delegation;
+
+public class SigningDelegationServiceTests
+{
+    [Fact]
+    public async Task RevokeSigneeRights_RevokeSigneeRights()
+    {
+        // Arrange
+        var accessManagementClient = new Mock<IAccessManagementClient>();
+        var logger = new Mock<ILogger<SigningDelegationService>>();
+        var service = new SigningDelegationService(accessManagementClient.Object, logger.Object);
+        var taskId = "taskId";
+        var instanceId = "instanceOwnerPartyId/instanceGuid";
+        var delegatorParty = new Party();
+        var appIdentifier = new AppIdentifier("testOrg", "testApp");
+        var signeeContexts = new List<SigneeContext>()
+        {
+            new()
+            {
+                TaskId = taskId,
+                Party = new Party(),
+                SigneeState = new SigneeState() { IsAccessDelegated = true },
+            },
+        };
+        var ct = new CancellationToken();
+
+        // Act
+        (signeeContexts, var success) = await service.RevokeSigneeRights(
+            taskId,
+            instanceId,
+            delegatorParty,
+            appIdentifier,
+            signeeContexts,
+            ct
+        );
+
+        // Assert
+        Assert.True(success);
+        Assert.False(signeeContexts[0].SigneeState.IsAccessDelegated);
+    }
+
+    [Fact]
+    public async Task RevokeSigneeRights_SigneeStateIsNotDelegated()
+    {
+        // Arrange
+        var accessManagementClient = new Mock<IAccessManagementClient>();
+        accessManagementClient
+            .Setup(x => x.RevokeRights(It.IsAny<DelegationRequest>(), It.IsAny<CancellationToken>()))
+            .Verifiable();
+        var logger = new Mock<ILogger<SigningDelegationService>>();
+        var service = new SigningDelegationService(accessManagementClient.Object, logger.Object);
+        var taskId = "taskId";
+        var instanceId = "instanceOwnerPartyId/instanceGuid";
+        var delegatorParty = new Party();
+        var appIdentifier = new AppIdentifier("testOrg", "testApp");
+        var signeeContexts = new List<SigneeContext>()
+        {
+            new()
+            {
+                TaskId = taskId,
+                Party = new Party(),
+                SigneeState = new SigneeState() { IsAccessDelegated = false }, // Signee is not delegated signing rights
+            },
+        };
+        var ct = new CancellationToken();
+
+        // Act
+        (signeeContexts, var success) = await service.RevokeSigneeRights(
+            taskId,
+            instanceId,
+            delegatorParty,
+            appIdentifier,
+            signeeContexts,
+            ct
+        );
+
+        // Assert
+        Assert.True(success);
+        Assert.False(signeeContexts[0].SigneeState.IsAccessDelegated);
+        accessManagementClient.Verify(
+            x => x.RevokeRights(It.IsAny<DelegationRequest>(), It.IsAny<CancellationToken>()),
+            Times.Never // No rights should be revoked, as the signee is not delegated signing rights in the first place
+        );
+    }
+
+    [Fact]
+    public async Task DelegateSigneeRights_DelegateSigneeRights()
+    {
+        // Arrange
+        var accessManagementClient = new Mock<IAccessManagementClient>();
+        var logger = new Mock<ILogger<SigningDelegationService>>();
+        var service = new SigningDelegationService(accessManagementClient.Object, logger.Object);
+        var taskId = "taskId";
+        var instanceId = "instanceOwnerPartyId/instanceGuid";
+        var delegatorParty = new Party();
+        var appIdentifier = new AppIdentifier("testOrg", "testApp");
+        var signeeContexts = new List<SigneeContext>()
+        {
+            new()
+            {
+                TaskId = taskId,
+                Party = new Party(),
+                SigneeState = new SigneeState() { IsAccessDelegated = false },
+            },
+        };
+        var ct = new CancellationToken();
+
+        // Act
+        (signeeContexts, var success) = await service.DelegateSigneeRights(
+            taskId,
+            instanceId,
+            delegatorParty,
+            appIdentifier,
+            signeeContexts,
+            ct
+        );
+
+        // Assert
+        Assert.True(success);
+        Assert.True(signeeContexts[0].SigneeState.IsAccessDelegated);
+    }
+
+    [Fact]
+    public async Task DelegateSigneeRights_WhenAlreadyDelegated_DoesNotInvokeClient()
+    {
+        // Arrange
+        var accessManagementClient = new Mock<IAccessManagementClient>();
+        accessManagementClient
+            .Setup(x => x.DelegateRights(It.IsAny<DelegationRequest>(), It.IsAny<CancellationToken>()))
+            .Verifiable();
+        var logger = new Mock<ILogger<SigningDelegationService>>();
+        var service = new SigningDelegationService(accessManagementClient.Object, logger.Object);
+        var taskId = "taskId";
+        var instanceId = "instanceOwnerPartyId/instanceGuid";
+        var delegatorParty = new Party();
+        var appIdentifier = new AppIdentifier("testOrg", "testApp");
+        var signeeContexts = new List<SigneeContext>()
+        {
+            new()
+            {
+                TaskId = taskId,
+                Party = new Party(),
+                SigneeState = new SigneeState() { IsAccessDelegated = true }, // Signee is already delegated signing rights
+            },
+        };
+        var ct = new CancellationToken();
+
+        // Act
+        (signeeContexts, var success) = await service.DelegateSigneeRights(
+            taskId,
+            instanceId,
+            delegatorParty,
+            appIdentifier,
+            signeeContexts,
+            ct
+        );
+
+        // Assert
+        Assert.True(success);
+        Assert.True(signeeContexts[0].SigneeState.IsAccessDelegated);
+        accessManagementClient.Verify(
+            x => x.DelegateRights(It.IsAny<DelegationRequest>(), It.IsAny<CancellationToken>()),
+            Times.Never // No rights should be delegated, as the signee already has signing rights
+        );
+    }
+}
