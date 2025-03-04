84e20e84097442a0a77a065844ac2d6ea61fddb7
HauklandJ <hauklandjohannes@gmail.com>
2025-03-04 16:22:17 +0100

    update notification choice logic

--- src/Altinn.App.Core/Features/Signing/Helpers/SigningCorrespondanceHelper.cs
+++ src/Altinn.App.Core/Features/Signing/Helpers/SigningCorrespondanceHelper.cs
@@ -12,26 +12,26 @@ internal static class SigningCorrespondenceHelper
     /// <returns>The notification choice as a <see cref="NotificationChoice"/>.</returns>
     internal static NotificationChoice GetNotificationChoice(Notification? notification)
     {
-        if (notification is null)
-        {
-            return NotificationChoice.None;
-        }
-
-        if (notification.Email is null && notification.Sms is null)
-        {
-            return NotificationChoice.None;
-        }
-
-        if (notification.Email is not null && notification.Sms is not null)
+        if (
+            notification?.Email is not null
+            && notification?.Email.EmailAddress is not null
+            && notification?.Sms is not null
+            && notification?.Sms.MobileNumber is not null
+        )
         {
             return NotificationChoice.SmsAndEmail;
         }
 
-        if (notification.Email is not null)
+        if (notification?.Email is not null && notification?.Email.EmailAddress is not null)
         {
             return NotificationChoice.Email;
         }
 
-        return NotificationChoice.Sms;
+        if (notification?.Sms is not null && notification?.Sms.MobileNumber is not null)
+        {
+            return NotificationChoice.Sms;
+        }
+
+        return NotificationChoice.None;
     }
 }
