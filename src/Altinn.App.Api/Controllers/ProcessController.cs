2d624904b9e76a75154dfcd3fbf6a56a9495d5f9
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2025-03-05 10:04:59 +0100

    Authorize process next as 'sign' when the process task type is signing and and no action was submitted. This is how storage authorizes it, and it means that the party that should run process next when everyone has signed, needs the 'sign' access right.

--- src/Altinn.App.Api/Controllers/ProcessController.cs
+++ src/Altinn.App.Api/Controllers/ProcessController.cs
@@ -746,10 +746,11 @@ public class ProcessController : ControllerBase
         {
             case "data":
             case "feedback":
-            case "signing":
                 return "write";
             case "confirmation":
                 return "confirm";
+            case "signing":
+                return "sign";
             default:
                 // Not any known task type, so assume it is an action type
                 return actionOrTaskType;
