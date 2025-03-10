1d9b2f0d1c60ebadd7f7434febd225271f5b08d7
Camilla Marie Dalan <camillamdalan@gmail.com>
2025-03-10 15:49:03 +0100

    removes redundant comment

--- src/Altinn.App.Api/Controllers/Providers/InternalControllerFeatureProvider.cs
+++ src/Altinn.App.Api/Controllers/Providers/InternalControllerFeatureProvider.cs
@@ -6,16 +6,6 @@ class InternalControllerFeatureProvider : ControllerFeatureProvider
 {
     protected override bool IsController(TypeInfo typeInfo)
     {
-        // https://source.dot.net/#Microsoft.AspNetCore.Mvc.Core/Controllers/ControllerFeatureProvider.cs,41
-        // contains
-        //
-        // // We only consider public top-level classes as controllers. IsPublic returns false for nested
-        // // classes, regardless of visibility modifiers
-        // if (!typeInfo.IsPublic)
-        // {
-        //   return false;
-        // }
-
         return typeInfo.Name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase)
             && typeInfo.IsDefined(typeof(ApiControllerAttribute))
             && typeof(ControllerBase).IsAssignableFrom(typeInfo);
