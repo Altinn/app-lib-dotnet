f855bd49447f42d3463a082224c3eede0e5d9621
Camilla Marie Dalan <camillamdalan@gmail.com>
2025-03-11 08:31:05 +0100

    inherit more of the original implementation of IsController from ControllerFeatureProvider

--- src/Altinn.App.Api/Controllers/Providers/InternalControllerFeatureProvider.cs
+++ src/Altinn.App.Api/Controllers/Providers/InternalControllerFeatureProvider.cs
@@ -4,10 +4,43 @@ using Microsoft.AspNetCore.Mvc.Controllers;
 
 class InternalControllerFeatureProvider : ControllerFeatureProvider
 {
+    private const string ControllerTypeNameSuffix = "Controller";
+
     protected override bool IsController(TypeInfo typeInfo)
     {
-        return typeInfo.Name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase)
-            && typeInfo.IsDefined(typeof(ApiControllerAttribute))
-            && typeof(ControllerBase).IsAssignableFrom(typeInfo);
+        if (!typeInfo.IsClass)
+        {
+            return false;
+        }
+
+        if (typeInfo.IsAbstract)
+        {
+            return false;
+        }
+
+        if (typeInfo.ContainsGenericParameters)
+        {
+            return false;
+        }
+
+        if (typeInfo.IsDefined(typeof(NonControllerAttribute)))
+        {
+            return false;
+        }
+
+        if (
+            !typeInfo.Name.EndsWith(ControllerTypeNameSuffix, StringComparison.OrdinalIgnoreCase)
+            && !typeInfo.IsDefined(typeof(ControllerAttribute))
+        )
+        {
+            return false;
+        }
+
+        if (!typeof(ControllerBase).IsAssignableFrom(typeInfo))
+        {
+            return false;
+        }
+
+        return true;
     }
 }
