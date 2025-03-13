d22efd35e7c75c362a76080eaf799ded2855834d
Ivar Nesje <ivarne@users.noreply.github.com>
2025-03-13 23:23:25 +0100

    Verify the actual response from api, instead of the parsed and potenitally modified response
    Use .ScrubMember("version") to remove the nuget version from the info section
    
    Use Microsoft.OpenApi.Readers only to verify that the package don't have validations that break.

--- src/Altinn.App.Api/Controllers/CustomOpenApiController.cs
+++ src/Altinn.App.Api/Controllers/CustomOpenApiController.cs
@@ -53,9 +53,6 @@ public class CustomOpenApiController : Controller
         _schemaRepository = new SchemaRepository();
     }
 
-    internal static readonly OpenApiSpecVersion SpecVersion = OpenApiSpecVersion.OpenApi3_0;
-    internal static readonly OpenApiFormat SpecFormat = OpenApiFormat.Json;
-
     /// <summary>
     /// Generate the custom OpenAPI documentation for the app
     /// </summary>
@@ -122,7 +119,7 @@ public class CustomOpenApiController : Controller
         var walker = new OpenApiWalker(new SchemaPostVisitor());
         walker.Walk(document);
 
-        return Ok(document.Serialize(SpecVersion, SpecFormat));
+        return Ok(document.Serialize(OpenApiSpecVersion.OpenApi3_0, OpenApiFormat.Json));
     }
 
     private string GetIntroDoc(ApplicationMetadata appMetadata)
