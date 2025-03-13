51ae8659b0d11357da52bc648c94f6a5ff82ce31
Adam Haeger <adamgullerud@gmail.com>
2025-03-13 12:37:23 +0100

    wip

--- src/Altinn.App.Api/Controllers/ResourceController.cs
+++ src/Altinn.App.Api/Controllers/ResourceController.cs
@@ -1,6 +1,8 @@
 using Altinn.App.Core.Helpers;
 using Altinn.App.Core.Internal.App;
+using Altinn.App.Core.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Altinn.App.Api.Controllers;
 
@@ -19,6 +21,32 @@ public class ResourceController : ControllerBase
     public ResourceController(IAppResources appResourcesService)
     {
         _appResourceService = appResourcesService;
+    }
+
+    /// <summary>
+    /// Get the json schema for the model
+    /// </summary>
+    /// <param name="id">Unique identifier of the model to fetch json schema for.</param>
+    /// <returns>The model json schema.</returns>
+    [HttpGet]
+    [Route("{org}/{app}/api/layouts/all-settings/{id}")]
+    public ActionResult<AllLayoutSettingsDTO> GetAll([FromRoute] string id)
+    {
+        var response = new AllLayoutSettingsDTO();
+        string layouts = _appResourceService.GetLayoutsForSet(id);
+
+        response.Layouts = layouts;
+
+        string? settings = _appResourceService.GetLayoutSettingsStringForSet(id);
+
+        response.Settings = settings;
+
+        // string? settings = _appResourceService.GetLayoutSettingsString();
+        //
+        // response.Settings = settings;
+        // JsonConvert.DeserializeObject(response)
+
+        return Ok(response);
     }
 
     /// <summary>
---
+++ src/Altinn.App.Core/Models/AllLayoutSettingsDTO.cs
@@ -0,0 +1,10 @@
+namespace Altinn.App.Core.Models;
+
+public class AllLayoutSettingsDTO
+{
+    public string Layouts { get; set; }
+
+    public string Schema { get; set; }
+
+    public string? Settings { get; set; }
+}
