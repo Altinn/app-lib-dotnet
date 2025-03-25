2c71eb2a8332a12a0619be996530a78c42da41a7
Daniel Skovli <daniel.skovli@gmail.com>
2025-03-25 08:21:56 +0100

    Housekeeping

--- src/Altinn.App.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/Altinn.App.Api/Extensions/ServiceCollectionExtensions.cs
@@ -13,7 +13,6 @@ using Altinn.App.Core.Extensions;
 using Altinn.App.Core.Features;
 using Altinn.App.Core.Features.Cache;
 using Altinn.App.Core.Features.Correspondence.Extensions;
-// using Altinn.App.Core.Features.FiksIO.Extensions;
 using Altinn.App.Core.Features.Maskinporten;
 using Altinn.App.Core.Features.Maskinporten.Extensions;
 using Altinn.App.Core.Features.Maskinporten.Models;
@@ -110,7 +109,6 @@ public static class ServiceCollectionExtensions
         // due to a bug in app insights: https://github.com/microsoft/ApplicationInsights-dotnet/issues/2828
         services.AddMaskinportenClient();
         services.AddCorrespondenceClient();
-        // services.AddFiksIOClient();
 
         AddAuthenticationScheme(services, config, env);
         AddAuthorizationPolicies(services);
