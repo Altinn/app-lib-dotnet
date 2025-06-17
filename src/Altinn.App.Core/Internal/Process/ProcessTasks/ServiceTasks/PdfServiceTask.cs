9ec36c7bd72799c53fa455f5a8ab8353ae9226e5
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2025-06-17 14:00:41 +0200

    Add support for service tasks, and add new service tasks for pdf generation and eFormidling.

---
+++ src/Altinn.App.Core/Internal/Process/ProcessTasks/ServiceTasks/PdfServiceTask.cs
@@ -0,0 +1,74 @@
+using Altinn.App.Core.Internal.App;
+using Altinn.App.Core.Internal.Pdf;
+using Altinn.App.Core.Internal.Process.Elements.AltinnExtensionProperties;
+using Altinn.Platform.Storage.Interface.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Altinn.App.Core.Internal.Process.ProcessTasks.ServiceTasks;
+
+internal interface IPdfServiceTask : IServiceTask { }
+
+/// <summary>
+/// Service task that generates PDFs for tasks specified in the process configuration.
+/// </summary>
+public class PdfServiceTask : IPdfServiceTask
+{
+    private readonly IPdfService _pdfService;
+    private readonly IProcessReader _processReader;
+    private readonly ILogger<PdfServiceTask> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PdfServiceTask"/> class.
+    /// </summary>
+    public PdfServiceTask(IPdfService pdfService, IProcessReader processReader, ILogger<PdfServiceTask> logger)
+    {
+        _logger = logger;
+        _pdfService = pdfService;
+        _processReader = processReader;
+    }
+
+    /// <inheritdoc />
+    public string Type => "pdf";
+
+    /// <inheritdoc/>
+    public async Task Execute(string taskId, Instance instance, CancellationToken cancellationToken = default)
+    {
+        _logger.LogDebug("Calling PdfService for PDF Service Task {TaskId}.", taskId);
+
+        ValidAltinnPdfConfiguration config = GetValidAltinnPdfConfiguration(taskId);
+        await _pdfService.GenerateAndStorePdf(instance, taskId, config.Filename, cancellationToken);
+
+        _logger.LogDebug("Successfully called PdfService for PDF Service Task {TaskId}.", taskId);
+    }
+
+    /// <inheritdoc />
+    public Task Start(string taskId, Instance instance)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public Task End(string taskId, Instance instance)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public Task Abandon(string taskId, Instance instance)
+    {
+        return Task.CompletedTask;
+    }
+
+    private ValidAltinnPdfConfiguration GetValidAltinnPdfConfiguration(string taskId)
+    {
+        AltinnTaskExtension? altinnTaskExtension = _processReader.GetAltinnTaskExtension(taskId);
+        AltinnPdfConfiguration? pdfConfiguration = altinnTaskExtension?.PdfConfiguration;
+
+        if (pdfConfiguration == null)
+        {
+            throw new ApplicationConfigException("PdfConfig is missing in the PDF service task configuration.");
+        }
+
+        return pdfConfiguration.Validate();
+    }
+}
