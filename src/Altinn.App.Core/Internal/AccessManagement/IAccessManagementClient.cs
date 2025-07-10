cbc01fc9f71780a119d5fb2885cb65ed59a807cf
Johannes Haukland <42615991+HauklandJ@users.noreply.github.com>
2025-07-10 09:40:23 +0200

    Update src/Altinn.App.Core/Internal/AccessManagement/IAccessManagementClient.cs
    Pr feedback
    
    Co-authored-by: Ivar Nesje <ivarne@users.noreply.github.com>

--- src/Altinn.App.Core/Internal/AccessManagement/IAccessManagementClient.cs
+++ src/Altinn.App.Core/Internal/AccessManagement/IAccessManagementClient.cs
@@ -5,5 +5,5 @@ namespace Altinn.App.Core.Internal.AccessManagement;
 internal interface IAccessManagementClient
 {
     public Task<DelegationResponse> DelegateRights(DelegationRequest delegation, CancellationToken ct);
-    public Task<DelegationResponse> RevokeRights(DelegationRequest delegation, CancellationToken ct);
+    public Task<DelegationResponse> RevokeRights(DelegationRequest delegation, CancellationToken ct = default);
 }
