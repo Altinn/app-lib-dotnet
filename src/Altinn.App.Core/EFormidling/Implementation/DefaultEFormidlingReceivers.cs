168e18494a0d4995f9a66a6763ed2786f0403f6f
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2025-09-29 10:14:46 +0200

    Avoid breaking change in IEFormidlingReceivers.

--- src/Altinn.App.Core/EFormidling/Implementation/DefaultEFormidlingReceivers.cs
+++ src/Altinn.App.Core/EFormidling/Implementation/DefaultEFormidlingReceivers.cs
@@ -1,4 +1,6 @@
 using Altinn.App.Core.EFormidling.Interface;
+using Altinn.App.Core.Internal.App;
+using Altinn.App.Core.Models;
 using Altinn.Common.EFormidlingClient.Models.SBD;
 using Altinn.Platform.Storage.Interface.Models;
 
@@ -9,8 +11,36 @@ namespace Altinn.App.Core.EFormidling.Implementation;
 /// </summary>
 public class DefaultEFormidlingReceivers : IEFormidlingReceivers
 {
+    private readonly IAppMetadata _appMetadata;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DefaultEFormidlingReceivers"/> class.
+    /// </summary>
+    /// <param name="appMetadata">Service for fetching application metadata</param>
+    public DefaultEFormidlingReceivers(IAppMetadata appMetadata)
+    {
+        _appMetadata = appMetadata;
+    }
+
     /// <inheritdoc />
-    public Task<List<Receiver>> GetEFormidlingReceivers(Instance instance, string? receiverFromConfig = null)
+    public async Task<List<Receiver>> GetEFormidlingReceivers(Instance instance)
+    {
+        ArgumentNullException.ThrowIfNull(instance);
+
+        ApplicationMetadata appMetadata = await _appMetadata.GetApplicationMetadata();
+
+        if (string.IsNullOrWhiteSpace(appMetadata.EFormidling?.Receiver))
+        {
+            return new List<Receiver>();
+        }
+
+        string receiver = appMetadata.EFormidling.Receiver.Trim();
+
+        return CreateReceiverList(receiver);
+    }
+
+    /// <inheritdoc />
+    public Task<List<Receiver>> GetEFormidlingReceivers(Instance instance, string? receiverFromConfig)
     {
         ArgumentNullException.ThrowIfNull(instance);
 
@@ -19,15 +49,20 @@ public class DefaultEFormidlingReceivers : IEFormidlingReceivers
             return Task.FromResult(new List<Receiver>());
         }
 
+        string receiver = receiverFromConfig.Trim();
+
+        return Task.FromResult(CreateReceiverList(receiver));
+    }
+
+    private static List<Receiver> CreateReceiverList(string receiver)
+    {
         var identifier = new Identifier
         {
             // 0192 prefix for all Norwegian organisations.
-            Value = $"0192:{receiverFromConfig.Trim()}",
+            Value = $"0192:{receiver}",
             Authority = "iso6523-actorid-upis",
         };
 
-        var receiver = new Receiver { Identifier = identifier };
-
-        return Task.FromResult<List<Receiver>>([receiver]);
+        return [new Receiver { Identifier = identifier }];
     }
 }
