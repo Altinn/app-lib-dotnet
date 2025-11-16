af99a5e2021c4eea5a55c699c3ac55b641fe499a
Ivar Nesje <ivarne@users.noreply.github.com>
2025-11-16 22:44:38 +0100

    Improve MocedServiceColletcition with Otel traces, logs and metrics
    * Use OpenTelemetry.Exporter.InMemory
    * Add a bare bones visualizer to join traces and logs in a single ascii tree view

---
+++ test/Altinn.App.Tests.Common/Utils/OtelVisualizers.cs
@@ -0,0 +1,92 @@
+using System.Diagnostics;
+using System.Text;
+using OpenTelemetry.Logs;
+
+namespace Altinn.App.Tests.Common.Utils;
+
+public static class OtelVisualizers
+{
+    public static string VisualizeActivities(
+        IEnumerable<Activity> activities,
+        IEnumerable<LogRecord> logs,
+        bool includeDuration = true,
+        int initialIndent = 0
+    )
+    {
+        var logsByActivityId = logs.GroupBy(l => l.SpanId)
+            .ToDictionary(k => k.Key, v => v.OrderBy(l => l.Timestamp).ToList());
+        var activityByParentId = activities
+            .GroupBy(a => a.ParentSpanId)
+            .ToDictionary(k => k.Key, g => g.OrderBy(a => a.StartTimeUtc).ToList());
+
+        var sb = new StringBuilder();
+        if (activityByParentId.TryGetValue(default, out var rootActivities))
+        {
+            foreach (var activity in rootActivities)
+            {
+                VisualizeActivity(sb, activityByParentId, logsByActivityId, activity, initialIndent, includeDuration);
+            }
+        }
+        else
+        {
+            sb.AppendLine("No root activities found.");
+        }
+
+        var remainingLogs = logsByActivityId.Values.SelectMany(l => l).OrderBy(l => l.Timestamp).ToList();
+        if (remainingLogs.Count > 0)
+        {
+            sb.AppendLine();
+            sb.AppendLine("Orphaned Logs:");
+            foreach (var log in remainingLogs)
+            {
+                sb.AppendLine($"[{log.LogLevel}] {log.FormattedMessage}");
+            }
+        }
+        return sb.ToString();
+    }
+
+    private static void VisualizeActivity(
+        StringBuilder sb,
+        Dictionary<ActivitySpanId, List<Activity>> activityByParentId,
+        Dictionary<ActivitySpanId, List<LogRecord>> logsByActivityId,
+        Activity activity,
+        int indent,
+        bool includeDuration
+    )
+    {
+        sb.Append(' ', indent);
+        if (includeDuration)
+        {
+            sb.AppendLine($"{activity.DisplayName}  [Duration: {activity.Duration.TotalMilliseconds} ms]");
+        }
+        else
+        {
+            sb.AppendLine($"{activity.DisplayName}");
+        }
+        foreach (var (key, value) in activity.Tags)
+        {
+            sb.Append(' ', indent + 1);
+            sb.AppendLine($"{key}: {value}");
+        }
+
+        if (activityByParentId.TryGetValue(activity.SpanId, out var children))
+        {
+            foreach (var child in children)
+            {
+                VisualizeActivity(sb, activityByParentId, logsByActivityId, child, indent + 2, includeDuration);
+            }
+        }
+
+        if (logsByActivityId.TryGetValue(activity.SpanId, out var logs) && logs.Count > 0)
+        {
+            sb.Append(' ', indent + 1);
+            sb.AppendLine("Logs:");
+            foreach (var log in logs)
+            {
+                sb.Append(' ', indent + 2);
+                sb.AppendLine($"[{log.LogLevel}] {log.FormattedMessage}");
+            }
+            logsByActivityId.Remove(activity.SpanId);
+        }
+    }
+}
