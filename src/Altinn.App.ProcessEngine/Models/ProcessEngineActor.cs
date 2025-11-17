b60768bf9e8300c85c5760ff8660a88998e5491d
Daniel Skovli <daniel.skovli@gmail.com>
2025-11-17 12:22:57 +0100

    Renames actor identifier to make usage clearer, for the time being
    This will probably be replaced at some point.

--- src/Altinn.App.ProcessEngine/Models/ProcessEngineActor.cs
+++ src/Altinn.App.ProcessEngine/Models/ProcessEngineActor.cs
@@ -1,8 +1,9 @@
 namespace Altinn.App.ProcessEngine.Models;
 
+// TODO: UserIdOrOrgNumber should probably be represented by a more specific type here. Eg. `Authenticated` or similar.
 /// <summary>
 /// Represents the user/entity on whose behalf the process engine is executing tasks.
 /// </summary>
-/// <param name="Identifier">The actor's identifier, a UserId/PartyId or similar</param>
+/// <param name="UserIdOrOrgNumber">The user-id or org number of the actor.</param>
 /// <param name="Language">Optional language code to associate with actions on behalf of this actor</param>
-public sealed record ProcessEngineActor(string Identifier, string? Language = null);
+public sealed record ProcessEngineActor(string UserIdOrOrgNumber, string? Language = null);
