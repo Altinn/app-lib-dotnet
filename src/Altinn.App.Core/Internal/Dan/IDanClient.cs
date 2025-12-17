597635ee51da40f8683148b36997bed2cf1d1f4f
PERHOF <pernille.hofgaard@hotmail.com>
2025-12-17 11:26:19 +0100

    added xml comments to classes, methods and properties

--- src/Altinn.App.Core/Internal/Dan/IDanClient.cs
+++ src/Altinn.App.Core/Internal/Dan/IDanClient.cs
@@ -1,6 +1,15 @@
 ﻿namespace Altinn.App.Core.Internal.Dan;
 
+/// <summary>
+/// DanClient interface
+/// </summary>
 public interface IDanClient
 {
+    /// <summary>
+    /// Method for getting a selected dataset from Dan Api
+    /// </summary>
+    /// <param name="dataset"></param>
+    /// <param name="subject"></param>
+    /// <returns></returns>
     public Task<Dictionary<string, string>> GetDataset(string dataset, string subject);
 }
