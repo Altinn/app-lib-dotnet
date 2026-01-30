78ae47b0c6dc99bcb6d512dc7a250a8c77e7d641
Bjørn Tore Gjerde <btgjerde@gmail.com>
2026-01-30 09:57:39 +0100

    DI

---
+++ src/Altinn.App.Core/Internal/WorkflowEngine/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,58 @@
+using Altinn.App.Core.Internal.WorkflowEngine.Commands;
+using Altinn.App.Core.Internal.WorkflowEngine.Commands.AltinnEvents;
+using Altinn.App.Core.Internal.WorkflowEngine.Commands.ProcessNext.ProcessEnd;
+using Altinn.App.Core.Internal.WorkflowEngine.Commands.ProcessNext.TaskAbandon;
+using Altinn.App.Core.Internal.WorkflowEngine.Commands.ProcessNext.TaskEnd;
+using Altinn.App.Core.Internal.WorkflowEngine.Commands.ProcessNext.TaskStart;
+using Altinn.App.Core.Internal.WorkflowEngine.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Altinn.App.Core.Internal.WorkflowEngine.DependencyInjection;
+
+internal static class ServiceCollectionExtensions
+{
+    internal static void AddWorkflowEngineIntegration(this IServiceCollection services)
+    {
+        // Process engine callback helpers
+        services.AddTransient<ProcessTaskResolver>();
+        services.AddTransient<ProcessNextRequestFactory>();
+        services.AddHttpClient<IProcessEngineClient, ProcessEngineClient>();
+
+        // Process engine callback handlers - TaskStart
+        services.AddTransient<IWorkflowEngineCommand, CommonTaskInitialization>();
+        services.AddTransient<IWorkflowEngineCommand, WorkflowTaskStart>();
+        services.AddTransient<IWorkflowEngineCommand, OnTaskStartingHook>();
+        services.AddTransient<IWorkflowEngineCommand, WorkflowTaskStartLegacyHook>();
+        services.AddTransient<IWorkflowEngineCommand, UnlockTaskData>();
+
+        // Process engine callback handlers - TaskAbandon
+        services.AddTransient<IWorkflowEngineCommand, WorkflowTaskAbandon>();
+        services.AddTransient<IWorkflowEngineCommand, OnTaskAbandonHook>();
+        services.AddTransient<IWorkflowEngineCommand, AbandonTaskLegacyHook>();
+
+        // Process engine callback handlers - TaskEnd
+        services.AddTransient<IWorkflowEngineCommand, CommonTaskFinalization>();
+        services.AddTransient<IWorkflowEngineCommand, WorkflowTaskEnd>();
+        services.AddTransient<IWorkflowEngineCommand, OnTaskEndingHook>();
+        services.AddTransient<IWorkflowEngineCommand, EndTaskLegacyHook>();
+        services.AddTransient<IWorkflowEngineCommand, LockTaskData>();
+
+        // Process engine callback handlers - ServiceTask
+        services.AddTransient<IWorkflowEngineCommand, ExecuteServiceTask>();
+
+        // Process engine callback handlers - ProcessEnd
+        services.AddTransient<IWorkflowEngineCommand, OnWorkflowEndingHook>();
+        services.AddTransient<IWorkflowEngineCommand, WorkflowEndLegacyHook>();
+
+        // Process engine callback handlers - State Management
+        services.AddTransient<IWorkflowEngineCommand, UpdateProcessStateInStorage>();
+
+        // Process engine callback handlers - Altinn Events
+        services.AddTransient<IWorkflowEngineCommand, CompletedAltinnEvent>();
+        services.AddTransient<IWorkflowEngineCommand, InstanceCreatedAltinnEvent>();
+        services.AddTransient<IWorkflowEngineCommand, MovedToAltinnEvent>();
+
+        // Validate all commands are registered
+        ProcessEngineCommandValidator.Validate(services);
+    }
+}
