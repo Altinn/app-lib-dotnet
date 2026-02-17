9dc91f81ded9f2c928b3ee4e9c634a215f01a2b5
HauklandJ <hauklandjohannes@gmail.com>
2026-02-17 09:31:53 +0100

    feat: add xmlelement attributes to the notification configuration

--- src/Altinn.App.Core/Internal/Process/Elements/AltinnExtensionProperties/AltinnNotificationConfiguration.cs
+++ src/Altinn.App.Core/Internal/Process/Elements/AltinnExtensionProperties/AltinnNotificationConfiguration.cs
@@ -3,16 +3,29 @@ using Altinn.App.Core.Features.Notifications;
 
 namespace Altinn.App.Core.Internal.Process.Elements.AltinnExtensionProperties;
 
-public class AltinnNotificationConfiguration
+/// <summary>
+/// Configuration properties for notifications in a process task.
+/// </summary>
+public sealed class AltinnNotificationConfiguration
 {
     /// Optionally set a notification provider that should be used for sending notifications related to this task.
     /// The notification provider with a matching ID must be registered as a transient service in the DI container.
     ///
     /// The provider must be an implementation of <see cref="INotificationProvider"/>
+    [XmlElement("notificationProviderId", Namespace = "http://altinn.no/process")]
     public string? NotificationProviderId { get; set; }
 
+    /// <summary>
+    /// Configuration for sending SMS notifications. If not set, no SMS notifications will be sent.
+    /// </summary>
+    [XmlElement("smsConfig", Namespace = "http://altinn.no/process")]
     public SmsConfig? SmsConfig { get; set; }
 
+
+    /// <summary>
+    /// Configuration for sending email notifications. If not set, no email notifications will be sent.
+    /// </summary>
+    [XmlElement("emailConfig", Namespace = "http://altinn.no/process")]
     public EmailConfig? EmailConfig { get; set; }
 
     internal ValidAltinnNotificationConfiguration Validate()
@@ -23,21 +36,51 @@ public class AltinnNotificationConfiguration
     }
 }
 
+/// <summary>
+/// Configuration for sending SMS notifications
+/// </summary>
 public class SmsConfig
 {
-    [XmlAttribute("SendSms")]
+    /// <summary>
+    /// Indicates whether an SMS should be sent or not. False by default.
+    /// </summary>
+    [XmlAttribute("sendSms")]
     public bool SendSms { get; set; } = false;
 
+    /// <summary>
+    /// The senders number to be used when sending the SMS.
+    /// </summary>
+    [XmlElement("senderNumber", Namespace = "http://altinn.no/process")]
     public string SenderNumber { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Text resource ID for the body of the SMS.
+    /// </summary>
+    [XmlElement("bodyTextResource", Namespace = "http://altinn.no/process")]
     public string BodyTextResource { get; set; } = string.Empty;
 }
 
+/// <summary>
+/// Configuration for sending email notifications
+/// </summary>
 public class EmailConfig
 {
-    [XmlAttribute("SendEmail")]
+    /// <summary>
+    /// Indicates whether an email should be sent or not. False by default.
+    /// </summary>
+    [XmlAttribute("sendEmail")]
     public bool SendEmail { get; set; } = false;
 
+    /// <summary>
+    /// Text resource ID for the subject of the email.
+    /// </summary>
+    [XmlElement("subjectTextResource", Namespace = "http://altinn.no/process")]
     public string SubjectTextResource { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Text resource ID for the body of the email.
+    /// </summary>
+    [XmlElement("bodyTextResource", Namespace = "http://altinn.no/process")]
     public string BodyTextResource { get; set; } = string.Empty;
 }
 
