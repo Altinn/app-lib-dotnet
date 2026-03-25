099cf7a541be8c7d88c775031aa68f0444c4a33c
Ivar Nesje <ivarne@users.noreply.github.com>
2026-03-25 22:09:20 +0100

    Make component expressions subform aware (#1698)
    Previously you could not use "component" expressions in subforms, because the context lookup did error because multiple contexts would match.

--- src/Altinn.App.Core/Internal/Expressions/ExpressionEvaluator.cs
+++ src/Altinn.App.Core/Internal/Expressions/ExpressionEvaluator.cs
@@ -260,7 +260,7 @@ public static partial class ExpressionEvaluator
             throw new ArgumentException("The component expression requires a component context");
         }
 
-        var targetContext = await state.GetComponentContext(context.Component?.PageId, componentId, context.RowIndices);
+        var targetContext = await state.GetComponentContext(componentId, relativeContext: context);
 
         if (targetContext is null)
         {
